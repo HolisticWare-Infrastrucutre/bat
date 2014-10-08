19b9b12cfd573156c146efc8456e3bb94477e570
moljac <mcvjetko@holisticware.net>
2014-10-08 08:57:54 +0200

    net use (mount) enhanced

--- 01-system-integration/net-use-mount-virtual-machine_on-macosx-setup.bat
+++ 01-system-integration/net-use-mount-virtual-machine_on-macosx-setup.bat
@@ -1,6 +1,23 @@
 @echo off
 
-net use p: "\\vmware-host\Shared Folders\Projects"
-net use x: "\\vmware-host\Shared Folders\exFATswprjs\www.holisticware.net"
+net use m: /delete
+net use p: /delete
+net use x: /delete
+	
+
+	
+	
+net use ^
+	m: "\\vmware-host\Shared Folders\moljac" ^
+	/persistent:yes
+	
+net use ^
+	p: "\\vmware-host\Shared Folders\Projects" ^
+	/persistent:yes
+	
+net use ^
+	x: "\\vmware-host\Shared Folders\exFATswprjs\www.holisticware.net" ^
+	/persistent:yes
+	
 
 @IF %ERRORLEVEL% NEQ 0 PAUSE	
