c5a7ac7508aae52a9da022bec666d640dbd74553
moljac <mcvjetko@holisticware.net>
2014-11-16 00:44:23 +0100

    path checking during clean

--- mac/clean.sh
+++ mac/clean.sh
@@ -13,13 +13,46 @@ fi
 
 df >> df-pre.txt
 
-find . -type d -name "Debug" -exec rm -rf {} \;
-find . -type d -name "Release" -exec rm -rf {} \;
+find . \
+		-type d -name "Debug" \
+		-not -path "*Dropbox*" \
+		-not -path "*Google Drive*" \
+		-exec rm -rf {} \;
+find . \
+		-type d -name "Release" \
+		-not -path "*Dropbox*" \
+		-not -path "*Google Drive*" \
+		-exec rm -rf {} \;
 
-find . -type f -name ".DS_Store" -exec rm -rf {} \;
-find . -type f -name "*.suo" -exec rm -rf {} \;
-find . -type f -name "*.sdf" -exec rm -rf {} \;
-find . -type f -name "*.csproj.user" -exec rm -rf {} \;
-find . -type f -name "*.cxproj.user" -exec rm -rf {} \;
+
+find . \
+		-type f -name ".DS_Store" \
+		-not -path "*Dropbox*" \
+		-not -path "*Google Drive*" \
+		-exec rm -f {} \;
+find . \
+		-type f -name "*.suo" \
+		-not -path "*Dropbox*" \
+		-not -path "*Google Drive*" \
+		-exec rm -f {} \;
+find . \
+		-type f -name "*.sdf" \
+		-not -path "*Dropbox*" \
+		-not -path "*Google Drive*" \
+		-exec rm -f {} \;
+find . \
+		-type f -name "*.csproj.user" 
+		-not -path "*Dropbox*" \
+		-not -path "*Google Drive*" \
+		-exec rm -f {} \;
+find . \
+		-type f -name "*.cxproj.user" \
+		-not -path "*Dropbox*" \
+		-not -path "*Google Drive*" \
+		-exec rm -f {} \;
+
+rm -fr ~/Library/Application\ Support/iPhone\ Simulator/6.1/Applications/*
+rm -fr ~/Library/Application\ Support/iPhone\ Simulator/7.1/Applications/*
+rm -fr ~/Library/Application\ Support/iPhone\ Simulator/7.0.3/Applications/*
 
 df >> df-post.txt
