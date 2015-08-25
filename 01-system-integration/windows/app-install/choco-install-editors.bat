57913fb071cac06c9178b9f299dd7ec19a800832
Miljenko Cvjetko moljac moki mel <mcvjetko@holisticware.net>
2015-08-25 15:14:35 +0200

    script fixes batch install

--- 01-system-integration/windows/app-install/choco-install-editors.bat
+++ 01-system-integration/windows/app-install/choco-install-editors.bat
@@ -1,19 +1,24 @@
 @echo off
 
-APPS=^
+set APPS=^
 	notepadplusplus-install ^
 	notepadplusplus-withuninstall ^
 	atom ^
 	brackets ^
 	sublimetext3 ^
-
 	
-for %%a IN (%APPS%) DO (
-	echo %%a
 	
-::	choco install ^
-)
+	
+::choco install %APPS%
+cinst -y -verbose ^
+	%APPS%
 
-
+:: for %%a IN (%APPS%) DO (
+:: 	echo %%a
+:: 	
+:: 	:: choco install %%a
+:: 	cinst -y -verbose ^
+:: 			%%a
+:: )
 
 @IF %ERRORLEVEL% NEQ 0 PAUSE	
