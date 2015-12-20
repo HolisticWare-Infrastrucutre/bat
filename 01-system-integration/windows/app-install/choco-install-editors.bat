3b4b98bd9ace38745be5e619d2c5fe1d503d239e
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2015-12-20 19:58:43 +0100

    editors update

--- 01-system-integration/windows/app-install/choco-install-editors.bat
+++ 01-system-integration/windows/app-install/choco-install-editors.bat
@@ -1,22 +1,21 @@
 @echo off
 
 set APPS=^
-	notepadplusplus-install ^
-	notepadplusplus-withuninstall ^
+	notepadplusplus ^
 	atom ^
 	brackets ^
 	sublimetext3 ^
-	
+
 ::choco install %APPS%
 cinst -y -verbose ^
 	%APPS%
 
 :: for %%a IN (%APPS%) DO (
 :: 	echo %%a
-:: 	
+::
 :: 	:: choco install %%a
 :: 	cinst -y -verbose ^
 :: 			%%a
 :: )
 
-@IF %ERRORLEVEL% NEQ 0 PAUSE	
+@IF %ERRORLEVEL% NEQ 0 PAUSE
