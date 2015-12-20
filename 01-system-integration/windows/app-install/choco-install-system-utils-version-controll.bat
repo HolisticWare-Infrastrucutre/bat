f9a54375dc4f7178c9501435983bc5e0e9ce7fe8
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2015-12-20 20:07:46 +0100

    browsers script version control

---
+++ 01-system-integration/windows/app-install/choco-install-system-utils-version-controll.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set APPS=^
+	msysgit ^
+	tortoisegit ^
+	tortoisesvn ^
+	tortoisehg ^
+	ccleaner ^
+	sysinternals ^
+	spybot ^
+	superantispyware ^
+	malwarebytes ^
+	fastcopy ^
+	windows-tweaker.install ^
+	daemontoolslite ^
+	ultraiso ^
+	magicdisc ^
+	isorecorder ^
+	virtualclonedrive ^
+	putty ^
+	superputty ^
+	googlechrome ^
+	chromium ^
+	googlechrome.canary ^
+	firefox ^
+
+
+::choco install %APPS%
+cinst -y -verbose ^
+	%APPS%
+
+for %%a IN (%APPS%) DO (
+	echo %%a
+
+	:: choco install %%a
+	cinst -y -verbose ^
+			%%a
+)
+
+@IF %ERRORLEVEL% NEQ 0 PAUSE
