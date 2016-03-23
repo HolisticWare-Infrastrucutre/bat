6947607b9bdfb6cbdab06f136fea7466fce6921b
moljac <mcvjetko@holisicware.net>
2016-03-23 22:23:18 +0100

    install prerequisites

---
+++ 01-system-integration/linux/corecrl-corefx-corecli-aspnet.core/fedora/01-install-prerequisites.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+
+sudo yum install -y git
+
+git config --global user.name  "moljac"
+git config --global user.email "mcvjetko@holisicware.net"
+
+git clone --recursive https://github.com/moljac/bat.git
+
