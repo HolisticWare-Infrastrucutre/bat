454c6e8d7ac5938ddb3ac9f05f1ad2d17e5264a3
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-03-23 15:10:52 +0100

    choco install editors

--- 01-system-integration/windows/app-install/choco-install-editors.bat
+++ 01-system-integration/windows/app-install/choco-install-editors.bat
@@ -5,10 +5,22 @@ set APPS=^
 	atom ^
 	brackets ^
 	sublimetext3 ^
-
+	visualstudicode ^
+	visualstudiocode-insiders ^
+	openinvscode ^
+	
+::===================================================================
 ::choco install %APPS%
-cinst -y -verbose ^
-	%APPS%
+set COMMAND=chinst --yes --force --verbose --debug
+
+:: chocolatey update packageName [-source ...] [-prerelease]
+::set COMMAND=cup  --yes --force --verbose --debug
+
+::choco uninstall %APPS%
+::set COMMAND=cuninst --yes --force --verbose --debug
+::===================================================================
+	
+%COMMAND% %APPS%
 
 :: for %%a IN (%APPS%) DO (
 :: 	echo %%a
