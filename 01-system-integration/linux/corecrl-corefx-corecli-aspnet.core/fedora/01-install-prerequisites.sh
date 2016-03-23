03da928b9f1759f11d9f3ab53d5606d49befc9bb
moljac <mcvjetko@holisicware.net>
2016-03-23 22:54:47 +0100

    prerequisites script update

--- 01-system-integration/linux/corecrl-corefx-corecli-aspnet.core/fedora/01-install-prerequisites.sh
+++ 01-system-integration/linux/corecrl-corefx-corecli-aspnet.core/fedora/01-install-prerequisites.sh
@@ -1,10 +1,50 @@
 #/bin/bash
 
+user_add_to_sufoers
+install_parallels_tools
+install_prerequisites
 
-sudo yum install -y git
+function install_prerequisites()
+{
+	sudo yum install -y \
+		git
 
-git config --global user.name  "moljac"
-git config --global user.email "mcvjetko@holisicware.net"
+	git config --global user.name  \
+		"moljac"
+	git config --global user.email \
+		"mcvjetko@holisicware.net"
 
-git clone --recursive https://github.com/moljac/bat.git
+	git clone --recursive https://github.com/moljac/bat.git
+
+}
+
+function user_add_to_sufoers()
+{
+	su
+	visudo
+	# add
+	# moljac    ALL=(ALL)      ALL
+}
+
+function install_parallels_tools()
+{
+
+	yum install -y \
+		gcc \
+		kernel-devel-$(uname -r) \
+		kernel-headers-$(uname -r) \
+		make \
+		checkpolicy \
+		selinux-policy-devel
+	eject /dev/sr0 
+	mkdir /media/cdrom/
+	mount -o exec /dev/sr0 /media/cdrom
+	cd /media/cdrom/
+	./install
+}
+
+
+
+
+
 
