6449dbe7f3d99b6316e7f467b6f5b2f6ae0a46d2
moljac <mcvjetko@holisicware.net>
2016-04-01 08:20:23 +0200

    lldb prerequisites

--- 01-system-integration/linux/corecrl-corefx-corecli-aspnet.core/linux/ubuntu/01-install-prerequisites.sh
+++ 01-system-integration/linux/corecrl-corefx-corecli-aspnet.core/linux/ubuntu/01-install-prerequisites.sh
@@ -1,11 +1,9 @@
 #/bin/bash
 
-install_prerequisites
+export PACKAGE_MANAGER=apt-get
 
 function install_prerequisites()
 {
-	PACKAGE_MANAGER=apt-get
-
 	sudo $PACKAGE_MANAGER install -y \
 		git
 
@@ -31,6 +29,20 @@ function install_prerequisites()
 		python \
 		libedit \
 
+	# lldb
+	sudo $PACKAGE_MANAGER install -y \
+		build-essential \
+		subversion \
+		libncurses5-dev
+		swig \
+		python2.7-dev \
+		libedit-dev \
+		doxygen \
+		graphviz \
+
+
+	sudo pip install epydoc # or install package python-epydoc
+
 	git config --global user.name  \
 		"moljac"
 	git config --global user.email \
@@ -40,71 +52,6 @@ function install_prerequisites()
 
 }
 
-function llvm_get()
-{
-	mkdir 	~/llvm
-	cd 	~/llvm
+install_prerequisites
 
-	rm -fr build/ 
-	rm -fr llvm*/ 
-	rm -fr *.tar.gz *.tar.xz
 
-	# From
-	# 	http://llvm.org/releases/download.html#3.2
-	# Download the latest version of
-	# 	LLVM sources 
-	#	including clang (C frontend) and 
-	#	compiler RT from 
-	wget http://llvm.org/releases/3.8.0/llvm-3.8.0.src.tar.xz
-	wget http://llvm.org/releases/3.8.0/cfe-3.8.0.src.tar.xz
-	wget http://llvm.org/releases/3.8.0/compiler-rt-3.8.0.src.tar.xz
-
-	# Extract the downloaded sources
-	tar xvf ./llvm-3.8.0.src.tar.xz
-	tar xvf ./cfe-3.8.0.src.tar.xz
-	tar xvf ./compiler-rt-3.8.0.src.tar.xz
-
-	mv ./llvm-3.8.0.src ./llvm-3.8.0
-	mv ./cfe-3.8.0.src ./clang
-	mv ./clang ./llvm-3.8.0/tools/
-	mv ./compiler-rt-3.8.0.src ./compiler-rt
-	mv ./compiler-rt ./llvm-3.8.0/projects/
-	
-	# Once everything is in place we create a separate folder for the build process
-	mkdir ./build
-	cd ./build
-
-	tree -d -L 1 ../llvm-3.8.0/tools/
-	tree -d -L 1 ../llvm-3.8.0/projects/
-	# 	|-- build (currently we are here)
-	#	|-- llvm-3.8.0
-	#	|   |-- projects
-	#	|   |   |-- compiler-rt
-	#	|   |-- tools
-	#	|   |   |-- clang
-	#	|-- cfe-3.8.0.src.tar.gz
-	#	|-- compiler-rt-3.8.0.src.tar.gz
-	#	|-- llvm-3.8.0.src.tar.gz
-
-	# Note : There are various configuration flags 
-	#	for CPU architecture, 
-	#	optimize builds, 
-	#	threads, etc. 
-	../llvm-3.8.0/configure --help
-	
-}
-
-function llvm_compile()
-{
-	# Now we start the actual configuration and compilation of LLVM 
-	# inside the 'build' folder outside of the main source directory 
-	# so as to keep the main source tree clean
-	../llvm-3.8.0/configure --enable-shared
-
-	time make -j 3
-}
-
-function llvm_check()
-{
-	make check-all
-}
