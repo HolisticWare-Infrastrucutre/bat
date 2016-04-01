d217e4f15a0668d45fe0cb2e45983cfd64245d95
moljac <mcvjetko@holisicware.net>
2016-04-01 08:16:49 +0200

    install prerequisites for fedora lldb

--- 01-system-integration/linux/corecrl-corefx-corecli-aspnet.core/linux/fedora/01-install-prerequisites.sh
+++ 01-system-integration/linux/corecrl-corefx-corecli-aspnet.core/linux/fedora/01-install-prerequisites.sh
@@ -2,10 +2,6 @@
 
 PACKAGE_MANAGER=yum
 
-user_add_to_sufoers
-install_parallels_tools
-install_prerequisites
-
 function install_prerequisites()
 {
 	PACKAGE_MANAGER=yum
@@ -24,12 +20,21 @@ function install_prerequisites()
 		gcc-c++ \
 		make \
 		cmake \
+		ninja-build \
 		glibc-devel \
 		glibc-devel.i686 \
 		swig \
 		python \
 		libedit \
 		
+		
+	# lldb
+	sudo $PACKAGE_MANAGER install -y \
+		libedit-devel \
+		libxml2-devel \
+		ncurses-devel \
+		python-devel \
+		swig \
 		
 
 	# 2016-03-21 gcc-c++ installs 
@@ -46,7 +51,7 @@ function install_prerequisites()
 
 }
 
-function user_add_to_sufoers()
+function user_add_to_sudoers()
 {
 	su
 	visudo
@@ -64,6 +69,8 @@ function install_parallels_tools()
 		make \
 		checkpolicy \
 		selinux-policy-devel
+
+
 	eject /dev/sr0 
 	mkdir /media/cdrom/
 	mount -o exec /dev/sr0 /media/cdrom
@@ -71,66 +78,11 @@ function install_parallels_tools()
 	./install
 }
 
-function llvm_compile()
-{
-	mkdir 	~/llvm
-	cd 	~/llvm
 
-	rm -fr build/ 
-	rm -fr llvm*/ 
-	rm -fr *.tar.gz *.tar.xz
+user_add_to_sufoers
+install_parallels_tools
+install_prerequisites
 
-	# From
-	# 	http://llvm.org/releases/download.html#3.2
-	# Download the latest version of
-	# 	LLVM sources 
-	#	including clang (C frontend) and 
-	#	compiler RT from 
-	wget http://llvm.org/releases/3.8.0/llvm-3.8.0.src.tar.xz
-	wget http://llvm.org/releases/3.8.0/cfe-3.8.0.src.tar.xz
-	wget http://llvm.org/releases/3.8.0/compiler-rt-3.8.0.src.tar.xz
-
-	# Extract the downloaded sources
-	tar xvf ./llvm-3.8.0.src.tar.xz
-	tar xvf ./cfe-3.8.0.src.tar.xz
-	tar xvf ./compiler-rt-3.8.0.src.tar.xz
-
-	mv ./llvm-3.8.0.src ./llvm-3.8.0
-	mv ./cfe-3.8.0.src ./clang
-	mv ./clang ./llvm-3.8.0/tools/
-	mv ./compiler-rt-3.8.0.src ./compiler-rt
-	mv ./compiler-rt ./llvm-3.8.0/projects/
-
-	# Once everything is in place we create a separate folder for the build process
-	mkdir ./build
-	cd ./build
-
-	tree -d -L 1 ../llvm-3.8.0/tools/
-	tree -d -L 1 ../llvm-3.8.0/projects/
-	# 	|-- build (currently we are here)
-	#	|-- llvm-3.8.0
-	#	|   |-- projects
-	#	|   |   |-- compiler-rt
-	#	|   |-- tools
-	#	|   |   |-- clang
-	#	|-- cfe-3.8.0.src.tar.gz
-	#	|-- compiler-rt-3.8.0.src.tar.gz
-	#	|-- llvm-3.8.0.src.tar.gz
-
-	# Note : There are various configuration flags 
-	#	for CPU architecture, 
-	#	optimize builds, 
-	#	threads, etc. 
-	../llvm-3.8.0/configure --help
-
-
-	# Now we start the actual configuration and compilation of LLVM 
-	# inside the 'build' folder outside of the main source directory 
-	# so as to keep the main source tree clean
-	../llvm-3.8.0/configure --enable-shared
-
-	time make -j 3
-}
 
 
 
