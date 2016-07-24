885e5cfb98a8af5285922a55949ab3f211120933
moljac <moljac@debian.holisticware.net>
2016-07-24 22:43:23 +0200

    debian

---
+++ 01-system-integration/linux/xamarin.android/debian/install-01-prerequisites.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+
+
+sudo apt-get install -y \
+    sudo
+
+nano /etc/sudoers
+
+
+sudo apt-get update -y 
+
+#-------------------------------------------------------------------------
+# guest additions install
+apt-cache search linux-headers-$(uname -r)
+
+sudo apt-get install -y \
+    linux-headers-$(uname -r) \
+    make \
+    gcc
+
+sudo sh /media/cdrom/autorun.sh
+#-------------------------------------------------------------------------
+
+sudo apt-get install -y \
+    git \
+
+#    gnome-tweak-tool \
+
+gsettings set org.gnome.desktop.background show-desktop-icons true
+
+
+git clone --recursive \
+    https://github.com/moljac/bat.git
+
+
