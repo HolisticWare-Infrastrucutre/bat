c0a330204dca1aaf63ba241fa959073e953b03db
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-08-10 08:56:22 +0200

    monodevelo / xamarin studio shell scripts

---
+++ 02-development/integrated-developmnet-environments/monodevelop-xamarin-studio/mac/components-cache-list.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ls 	-al 	~/Caches/Xamarin
+ls	-al		~/Library/Caches/Xamarin/*
+
+#ls 	-al 	~/Library/Caches/Xamarin/ComponentInfo/	
+#ls 	-al		~/Library/Caches/Xamarin/Components/
+
+ls 	-al		~/Library/Caches/Xamarin/Component*
