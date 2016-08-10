5e7ce79568f285b1521a92706095b9f808f3a3fe
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-08-10 08:54:12 +0200

    xamarin solution / monodevelop files moved

--- 02-development/monodevelop-xamarin-studio/mac/components-cache-list.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ls 	-al 	~/Caches/Xamarin
-ls	-al		~/Library/Caches/Xamarin/*
-
-#ls 	-al 	~/Library/Caches/Xamarin/ComponentInfo/	
-#ls 	-al		~/Library/Caches/Xamarin/Components/
-
-ls 	-al		~/Library/Caches/Xamarin/Component*
