9809465a82996a121bce1f34ac36fbb0a78cd004
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-08-19 13:41:20 +0200

    updated for android

--- 02-development/mobile/mac-osx-tools-react-native.sh
+++ 02-development/mobile/mac-osx-tools-react-native.sh
@@ -13,4 +13,12 @@ npm install -g react-native-cli
 react-native init AwesomeProject
 cd AwesomeProject
 react-native run-ios &
+
+
+VBoxManage list vms
+open \
+	-a /Applications/Genymotion.app/Contents/MacOS/player.app \
+		--args \
+		--vm-name '97542b95-0c7a-46ea-8015-53a58eb283a2'
+
 react-native run-android &
