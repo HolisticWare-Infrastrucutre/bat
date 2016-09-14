e75633a63486a961d76ee2b9cf5c1634f2471661
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-09-14 19:47:05 +0200

    cocoapods install

--- mac/environment-setup.sh
+++ mac/environment-setup.sh
@@ -17,3 +17,27 @@ defaults write com.apple.finder AppleShowAllFiles YES
 
 killall Finder
 /System/Library/CoreServices/Finder.app
+
+
+sudo gem install activesupport -v 4.2.6
+sudo gem install cocoapods
+pod
+
+/usr/bin/ruby -e \
+    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    
+brew install \
+    autoconf    \
+    dos2unix    \
+    gradle      \
+    node        \
+    openssl     \
+    pkg-config  \
+    watchman    \
+    automake    \
+    gettext     \
+    libtool     \
+    ntfs-3g     \
+    pcre        \
+    swig        \
