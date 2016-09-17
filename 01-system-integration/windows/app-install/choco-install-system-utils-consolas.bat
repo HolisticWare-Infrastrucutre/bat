49c8827f4d21892e8724343dcb8b207b6e32984f
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-09-17 23:53:57 +0200

    choco for consolas

---
+++ 01-system-integration/windows/app-install/choco-install-system-utils-consolas.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set APPS=^
+	console2 ^
+	consolez ^
+	
+
+::choco install %APPS%
+cinst -y -verbose ^
+	%APPS%
+
+for %%a IN (%APPS%) DO (
+	echo %%a
+
+	:: choco install %%a
+	cinst -y -verbose ^
+			%%a
+)
+
+@IF %ERRORLEVEL% NEQ 0 PAUSE
