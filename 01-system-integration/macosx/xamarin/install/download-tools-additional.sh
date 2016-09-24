f8915218abc51836add6283481107514d76d8f5a
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-09-24 15:52:59 +0200

    cmd-here

--- 01-system-integration/macosx/xamarin/install/download-tools-additional.sh
+++ 01-system-integration/macosx/xamarin/install/download-tools-additional.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-curl -v -L -C -O \
+curl -v -L -C - -O \
 	https://download.xamarin.com/objective-sharpie/ObjectiveSharpie.pkg
 
-curl -v -L -C -O \
+curl -v -L -C - -O \
 	https://download.xamarin.com/inspector/XamarinInteractive.pkg
 	# https://download.xamarin.com/inspector/XamarinInteractive.msi
 	
-curl -v -L -C -O \
+curl -v -L -C - -O \
 	https://download.xamarin.com/profiler/profiler-mac.pkg
 	# https://download.xamarin.com/profiler/profiler-windows.msi
 	
