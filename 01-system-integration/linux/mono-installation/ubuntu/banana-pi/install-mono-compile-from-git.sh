c71738fe23fa276b085975654e7f2e612569c0c3
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-09-24 15:53:59 +0200

    banana pi ubuntu mono install script

---
+++ 01-system-integration/linux/mono-installation/ubuntu/banana-pi/install-mono-compile-from-git.sh
@@ -0,0 +1,22 @@
+
+
+sudo apt-get update
+sudo apt-get install -y openssh-server
+
+
+ps -e | grep ssh
+/etc/init.d/ssh start
+
+
+sudo aptitude install -y \
+        make \
+        automake \
+        autoconf \
+        autogen \
+        libtool \
+        gcc \
+        g++ \
+	    bison \
+        gettext \
+
+
