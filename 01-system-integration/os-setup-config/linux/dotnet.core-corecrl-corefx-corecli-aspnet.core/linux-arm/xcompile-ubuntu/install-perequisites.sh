40edfb25909010e18e97d0ef135105570087e78c
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2016-10-07 19:25:59 +0200

    ubuntu prerequisites

--- 01-system-integration/os-setup-config/linux/dotnet.core-corecrl-corefx-corecli-aspnet.core/linux-arm/xcompile-ubuntu/install-perequisites.sh
+++ 01-system-integration/os-setup-config/linux/dotnet.core-corecrl-corefx-corecli-aspnet.core/linux-arm/xcompile-ubuntu/install-perequisites.sh
@@ -6,11 +6,13 @@ https://gist.github.com/preshing/41d5c7248dea16238b60
 https://github.com/dotnet/coreclr/blob/master/Documentation/building/cross-building.md
 
 https://github.com/dotnet/corert/blob/master/Documentation/cross-building.md
+https://github.com/dotnet/corert/issues?utf8=%E2%9C%93&q=is%3Aissue%20is%3Aopen%20arm
 
 https://github.com/dotnet/corefx/blob/master/Documentation/building/cross-building.md
 
 
 	sudo apt-get install -y \
+		llvm \
 		cmake \
 		qemu \
 		qemu-user-static \
@@ -22,6 +24,9 @@ https://github.com/dotnet/corefx/blob/master/Documentation/building/cross-buildi
 		binutils-arm-linux-gnueabihf \
 		binutils-aarch64-linux-gnu \
 		
+    mkdir dotnet/
+    cd dotnet/
+	
 	git clone --recursive \
         https://github.com/dotnet/corert.git
 	git clone --recursive \
@@ -29,12 +34,29 @@ https://github.com/dotnet/corefx/blob/master/Documentation/building/cross-buildi
 	git clone --recursive \
         https://github.com/dotnet/corefx.git
 
-		
-	sudo ./coreclr/cross/build-rootfs.sh arm 
-	ls -al ./coreclr/cross/rootfs/arm/
-	./coreclr/build.sh arm debug verbose clean cross
+	export MODULE=coreclr
+	export ARCHITECTURE=arm
+	sudo ./$MODULE/cross/build-rootfs.sh $ARCHITECTURE 
+	ls -al ./$MODULE/cross/rootfs/$ARCHITECTURE/
+	./$MODULE/build.sh $ARCHITECTURE debug verbose clean cross
+	#to generate the rootfs elsewhere:
+	#sudo ROOTFS_DIR=~/$MODULE-cross/$ARCHITECTURE ./$MODULE/cross/build-rootfs.sh $ARCHITECTURE
+	# ROOTFS_DIR=~/$MODULE-cross/$ARCHITECTURE ./$MODULE/build.sh $ARCHITECTURE debug verbose clean cross
 	
-	sudo ./coreclr/cross/build-rootfs.sh arm64
-	ls -al ./coreclr/rootfs/arm64/
-	./build.sh arm debug verbose clean cross
+	export ARCHITECTURE=arm64
+	sudo ./$MODULE/cross/build-rootfs.sh $ARCHITECTURE
+	ls -al ./$MODULE/rootfs/$ARCHITECTURE/
+	./$MODULE/build.sh $ARCHITECTURE debug verbose clean cross
+	
+	export MODULE=corert
+	export ARCHITECTURE=arm
+	sudo ./$MODULE/cross/build-rootfs.sh arm 
+	ls -al ./$MODULE/cross/rootfs/arm/
+	./$MODULE/build.sh arm debug verbose clean cross
+	
+	export MODULE=corefx
+	export ARCHITECTURE=arm
+	sudo ./$MODULE/cross/build-rootfs.sh arm 
+	ls -al ./$MODULE/cross/rootfs/arm/
+	./$MODULE/build.sh arm debug verbose clean cross
 	
