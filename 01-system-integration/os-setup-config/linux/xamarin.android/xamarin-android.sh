4fe2a3018a4b7a846273bf1ab7da6bfbc69114d7
Miljenko Cvjetko moljac moki mel <mcvjetko@holisticware.net>
2016-12-02 09:43:17 +0100

    flatpak runtime

--- 01-system-integration/os-setup-config/linux/xamarin.android/xamarin-android.sh
+++ 01-system-integration/os-setup-config/linux/xamarin.android/xamarin-android.sh
@@ -18,6 +18,8 @@ wget http://download.mono-project.com/test/XamarinStudio-6.1.0.5441.flatpak
 # Install it to your user account:
 flatpak install --user --bundle XamarinStudio-6.1.0.5441.flatpak
 
+flatpak install gnome org.gnome.Sdk 3.20
+flatpak list
 
 wget https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz 
 wget https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip
