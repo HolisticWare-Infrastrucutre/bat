5e738c030e5c4c54b2bff9021fdfc3fbfbafeb4c
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-01-06 22:20:57 +0100

    choco install via nuget

---
+++ windows/choco-install-nuget.bat
@@ -0,0 +1,11 @@
+
+
+vs_enterprise.exe ^
+	--quiet --noRestart ^
+	--installPath "C:\Program Files (x86)\Microsoft Visual Studio 15.0" ^
+	--add Microsoft.VisualStudio.Workload.ManagedDesktop ^
+	--add Microsoft.VisualStudio.Workload.NativeDesktop ^
+	--add Microsoft.VisualStudio.Component.TestTools.CodedUITest ^
+	--add Microsoft.VisualStudio.Component.TestTools.WebLoadTest ^
+	--add Microsoft.VisualStudio.Component.VSSDK ^
+	--includeOptional 
