b6ca45a80f77c0d5c9344d177a5856d9f8a38a5c
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-01-06 22:20:28 +0100

    mac osx download software

---
+++ mac/download/download-software.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# publishURL
+
+URLS=\
+"\
+    http://b1.org/ \
+    http://lightheadsw.com/caffeine/ \
+    http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/ \
+    https://inkscape.org/en/download/mac-os/ \
+    https://www.xquartz.org/ \
+    https://inkscape.org/en/download/mac-os/ \
+"    
+
+open $URLS
