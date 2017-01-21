5a091cc93aedb3890a86dc8c21968f7aeabf5f7d
Miljenko Cvjetko moljac moki mel <mcvjetko@holisticware.net>
2017-01-21 11:06:42 +0100

    slack gitter added

--- mac/download/download-software.sh
+++ mac/download/download-software.sh
@@ -7,6 +7,8 @@ URLS=\
     https://go.microsoft.com/fwlink/?LinkID=620882 \
     https://www.visualstudio.com/thank-downloading-visual-studio-mac/?sku=vsmac \
     https://www.xamarin.com/download \
+    https://slack.com/ssb/download-osx \
+    https://update.gitter.im/osx/latest \
     http://b1.org/ \
     http://lightheadsw.com/caffeine/ \
     http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/ \
