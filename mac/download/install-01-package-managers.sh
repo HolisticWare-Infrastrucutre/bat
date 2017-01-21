eb00482edd9a482bffc3fd36b3ff365dde15803f
Miljenko Cvjetko moljac moki mel <mcvjetko@holisticware.net>
2017-01-21 10:41:23 +0100

    curl for ports addded

--- mac/download/install-01-package-managers.sh
+++ mac/download/install-01-package-managers.sh
@@ -12,4 +12,7 @@ echo "Installing brew"
 echo "Installing ports (Macports)"
 open \
   https://guide.macports.org/chunked/installing.macports.html
+URL=https://github.com/macports/macports-base/releases/download/v2.3.5/MacPorts-2.3.5-10.12-Sierra.pkg
+curl -v -L -C - -O $URL
 #----------------------------------------------------------------------------------------------
+
