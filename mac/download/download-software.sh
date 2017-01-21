62f4bbb2aace0eb87418ddc07bcffa19d50f764a
Miljenko Cvjetko moljac moki mel <mcvjetko@holisticware.net>
2017-01-21 09:34:21 +0100

    visual studio links

--- mac/download/download-software.sh
+++ mac/download/download-software.sh
@@ -4,6 +4,8 @@
 
 URLS=\
 "\
+    https://go.microsoft.com/fwlink/?LinkID=620882 \
+    https://www.visualstudio.com/thank-downloading-visual-studio-mac/?sku=vsmac \
     http://b1.org/ \
     http://lightheadsw.com/caffeine/ \
     http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/ \
