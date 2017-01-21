2ab7144db6fff1335d071890ac82a1a81f7b590c
Miljenko Cvjetko moljac moki mel <mcvjetko@holisticware.net>
2017-01-21 10:58:17 +0100

    xamarin studio url added

--- mac/download/download-software.sh
+++ mac/download/download-software.sh
@@ -6,10 +6,10 @@ URLS=\
 "\
     https://go.microsoft.com/fwlink/?LinkID=620882 \
     https://www.visualstudio.com/thank-downloading-visual-studio-mac/?sku=vsmac \
+    https://www.xamarin.com/download \
     http://b1.org/ \
     http://lightheadsw.com/caffeine/ \
     http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/ \
-    https://inkscape.org/en/download/mac-os/ \
     https://www.xquartz.org/ \
     https://inkscape.org/en/download/mac-os/ \
 "    
