1ec439b1b35694519702ecb77f18e11d90c356b8
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-03-24 22:03:11 +0100

    mac download

--- mac/download/download-software.sh
+++ mac/download/download-software.sh
@@ -10,6 +10,7 @@ URLS=\
     https://inkscape.org/en/download/mac-os/ \
     https://www.xquartz.org/ \
     https://inkscape.org/en/download/mac-os/ \
+	http://launchpad.net/unetbootin/trunk/625/+download/unetbootin-mac-625.dmg \
 "    
 
 open $URLS
