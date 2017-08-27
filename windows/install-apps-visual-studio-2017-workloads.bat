5df86417fd77663fc54ef118fa7c4306a4523826
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-08-27 10:36:12 +0200

    vs2017 install

---
+++ windows/install-apps-visual-studio-2017-workloads.bat
@@ -0,0 +1,13 @@
+cinst -y netfx-4.5.2-devpack
+cinst -y visualstudio2017community
+cinst -y visualstudio2017buildtools
+cinst -y visualstudio2017-workload-netweb
+cinst -y visualstudio2017-workload-manageddesktop
+cinst -y visualstudio2017-workload-netcoretools
+cinst -y visualstudio2017-workload-webbuildtools
+cinst -y visualstudio2017-workload-azure
+cinst -y visualstudio2017testagent
+cinst -y visualstudio2017-workload-data
+cinst -y visualstudio2017-workload-node
+
+cinst -y git-credential-manager-for-windows
