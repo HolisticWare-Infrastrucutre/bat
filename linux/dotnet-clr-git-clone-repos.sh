65567f385463ca930b15214ece2fe94780e7911f
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-05-19 11:56:55 +0200

    dotnet repos clone script

---
+++ linux/dotnet-clr-git-clone-repos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+function git_clone_repos()
+{
+    git clone --recursive \
+        https://github.com/dotnet/corefx.git \
+        ~/~/git/corefx/
+
+    git clone --recursive \
+        https://github.com/dotnet/coreclr.git \
+        ~/~/git/coreclr/
+}
+
+git_clone_repos
