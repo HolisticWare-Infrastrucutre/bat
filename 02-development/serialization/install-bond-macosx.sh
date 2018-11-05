6ae135b396394907fed7316c8d662f1753bce9cb
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-11-05 23:49:46 +0100

    bond macosx

--- 02-development/serialization/install-bond-macosx.sh
+++ 02-development/serialization/install-bond-macosx.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # https://www.diycode.cc/projects/Microsoft/bond
+# https://github.com/Microsoft/bond
 
-brew update
-
+# add this to .bash_profile 
+#   NOTE - remove `\` chars
 echo "
-# Go development - needed for Serializztion Microsoft Bond
+# Go development - needed for Serialization Microsoft Bond
 export GOPATH=\"${HOME}/.go\"
 export GOROOT=\"$(brew --prefix golang)/libexec\"
 export PATH=\"$PATH:${GOPATH}/bin:${GOROOT}/bin\"
 test -d \"${GOPATH}" || mkdir "${GOPATH}\"
 "
+
+brew update
 
 brew install \
     cmake \
@@ -37,11 +40,6 @@ go get github.com/golang/lint/golint
 code --install-extension ms-vscode.Go
 
 
-export PYTHON_LIBRARY=/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/libpython3.7.dylib 
-
-cmake .. \
-    -DPYTHON_LIBRARY=/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/libpython3.7.dylib 
-
 sudo xcode-select --reset
 xcode-select --install
 sudo xcode-select --switch /Applications/Xcode.app
@@ -55,14 +53,24 @@ cabal install happy
 rm -fr bond/
 git clone --recursive https://github.com/Microsoft/bond.git
 cd bond
+
 mkdir build
 cd build
-cmake ..
+export PYTHON_LIBRARY=/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/libpython3.7.dylib \
+#cmake ..
+cmake .. \
+    -DPYTHON_LIBRARY=/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/libpython3.7.dylib 
 make
+make --jobs 8 check
 
+cd ..
+mkdir build-xcode
+cd build-xcode
+cmake .. \
+    -G Xcode .. \
+    -DPYTHON_LIBRARY=/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/libpython3.7.dylib 
 
-cmake -G Xcode ..
-
+make
 make --jobs 8 check
 
 sudo make install
