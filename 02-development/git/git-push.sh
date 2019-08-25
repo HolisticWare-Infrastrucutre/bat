0f57bbea56e6452cb1380eac7e1e7025deaaf9db
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-08-25 08:34:38 +0200

    damn newlines

--- 02-development/git/git-push.sh
+++ 02-development/git/git-push.sh
@@ -4,12 +4,26 @@ DIR_CURRENT=$(pwd)
 
 echo $DIR_CURRENT
 
-for DIR in $(ls);
+DIRS=$(ls)
+DIRS=$(find $DIR_CURRENT -type d)
+
+for DIR in $DIRS;
 do
-    echo $DIR
-    cd $DIR
-    git push --recurse-submodules=on-demand
-    git submodule foreach git push origin master
-    cd ..
+    if [ -d "$DIR/.git/" ]; 
+    then
+        echo git repo true = $DIR
+        #-----------------------------------------------
+        # pushd $DIR
+        # popd
+        #-----------------------------------------------
+        ( \
+            cd $DIR \
+            && \
+            git push --recurse-submodules=on-demand \
+            && \
+            git submodule foreach git push origin master \
+            cd - \
+        )
+    fi
 done
 
