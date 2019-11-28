3339bffb1d30f133995eed4e1b5995ba4c490aae
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-11-28 23:19:44 +0100

    dotnet version 3.0.0

--- 01-system-integration/dotnet-install.sh
+++ 01-system-integration/dotnet-install.sh
@@ -3,6 +3,7 @@
 wget https://dotnetwebsite.azurewebsites.net/download/dotnet-core/scripts/v1/dotnet-install.sh
 chmod +x ./dotnet-install.sh
 ./dotnet-install.sh -Channel LTS
+./dotnet-install.sh --runtime dotnet --version 3.0.0
 export PATH="$PATH:$HOME/.dotnet"
 
 echo 'export PATH="$PATH:$HOME/.dotnet"' >> ~/.bashrc
