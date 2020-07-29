f615f85a128b9c430535c2a038300479c4e9bc8e
moljac <mcvjetko@holisticware.net>
2020-07-29 09:45:20 +0200

    some detailed scripts added

---
+++ 02-development/mobile/xamarin.android/build-macosx-cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# build-macosx-cleanup.sh
+
+rm -fr $HOME/android-toolchain/
+rm -fr $HOME/android-archives/
---
+++ 02-development/mobile/xamarin.android/build-macosx-minimal-branch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# migration-to-sdk-short-style-projects
+# migration-to-sdk-short-style-projects-02
+
+export ANDROID_SDK_ROOT=$HOME/Library/Developer/Xamarin/android-sdk-macosx
+export AndroidSdkDirectory=
+git clone \
+    --branch migration-to-sdk-short-style-projects-02 \
+    https://github.com/xamarin/xamarin-android.git \
+    xamarin-android-sdk-style-projects-02
+
+cd xamarin-android-sdk-style-projects-02/
+make prepare
+make all
