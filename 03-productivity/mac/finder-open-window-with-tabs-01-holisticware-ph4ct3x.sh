4a6d91c47283ebf132c4b36b034774072b64647b
moljac <mcvjetko@holisticware.net>
2020-12-10 14:11:12 +0100

    productivity - open Finder window with given tabs

---
+++ 03-productivity/mac/finder-open-window-with-tabs-01-holisticware-ph4ct3x.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+osascript \
+    finder-open-window-with-tabs-01-holisticware-ph4ct3x.scpt
---
+++ 03-productivity/mac/finder-open-window-with-tabs.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+source \
+    finder-open-window-with-tabs-00-main.sh
+source \
+    finder-open-window-with-tabs-01-holisticware.sh
+source \
+    finder-open-window-with-tabs-01-holisticware-ph4ct3x.sh
+source \
+    ./finder-open-window-with-tabs-09-private.sh
