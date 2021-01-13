61cf630159b152ffc587d751a2b72a0853821c48
moljac <mcvjetko@holisticware.net>
2021-01-13 16:54:50 +0100

    renaming

---
+++ 02-development/mobile/xamarin.android/repositories/clone-msal-tw.sh
@@ -0,0 +1,26 @@
+﻿#!/bin/bash
+
+
+
+
+# clone repo branch to folder
+
+export FOLDER=msal-tw-01
+export BRANCH=trwalke/AndroidBinding
+
+git clone \
+    --branch $BRANCH \
+    https://github.com/xamarin/xamarin-android.git \
+    $FOLDER
+
+export FOLDER=msal-sample
+export BRANCH=master
+
+export FOLDER=msal-sample
+export BRANCH=master
+
+git clone \
+    --branch $BRANCH \
+    https://github.com/AzureAD/microsoft-authentication-library-for-dotnet.git \
+    $FOLDER
+
