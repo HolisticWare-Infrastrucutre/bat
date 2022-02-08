c70a9ce8126de7f9dce9a2777b7abb0ae75f973b
moljac <mcvjetko@holisticware.net>
2022-02-08 14:07:09 +0100

    Update install-dotnet-workloads.sh

--- 01-system-integration/mac/01-install/install-dotnet-workloads.sh
+++ 01-system-integration/mac/01-install/install-dotnet-workloads.sh
@@ -1,65 +1,66 @@
 #!/bin/zsh
 
+# https://github.com/dotnet/maui/blob/main/.github/DEVELOPMENT.md#net-maui-workload
+
+# https://aka.ms/dotnet/6.0.2xx/daily/dotnet-sdk-osx-x64.pkg
+
+dotnet tool list --global
+
 # export ACTION=search
 # export ACTION=uninstall
 export ACTION=install
 # export ACTION=update
 
 
-dotnet workload search android
-dotnet workload search ios     
-dotnet workload search macos
-dotnet workload search mac
-dotnet workload search tvos
-dotnet workload search maui
+export WORKLOADZ=\
+"
+android
+# android-aot
+ios     
+# mac
+macos
+maccatalyst
+maui
+maui-android
+maui-desktop
+maui-ios
+maui-maccatalyst
+maui-mobile
+maui-windows
+microsoft-android-sdk-full
+microsoft-ios-sdk-full
+microsoft-maccatalyst-sdk-full
+microsoft-macos-sdk-full
+microsoft-tvos-sdk-full
+tvos
+wasm-tools
+"
 
+IFS=$'\n'
+# ZSH does not split words by default (like other shells):
+setopt sh_word_split
 
+for WORKLOAD in $WORKLOADZ
+do
+    if [[ $WORKLOAD == "#"* ]]
+    then
+        continue
+    fi
 
-sudo \
-    dotnet workload $ACTION \
-        android \
-        android-aot \
+    echo Tool: $WORKLOAD
+    sudo \
+        dotnet workload \
+            $ACTION \
+            $WORKLOAD \
+            --from-rollback-file https://aka.ms/dotnet/maui/main.json \
+            --source https://aka.ms/dotnet6/nuget/index.json \
+            --source https://api.nuget.org/v3/index.json
 
-sudo \
-    dotnet workload $ACTION \
-        ios
+#    sudo \
+#        dotnet workload install \
+#            $WORKLOAD \
 
-sudo \
-    dotnet workload $ACTION \
-        macos \
-        maccatalyst \
+    sudo dotnet workload update
 
-sudo \
-    dotnet workload $ACTION \
-        tvos \
+done
 
-sudo \
-    dotnet workload $ACTION \
-        maui \
-        maui-android \
-        maui-desktop \
-        maui-ios \
-        maui-maccatalyst \
-        maui-mobile \
-        maui-windows \
-
-
-sudo \
-    dotnet workload $ACTION \
-        microsoft-android-sdk-full \
-        microsoft-ios-sdk-full \
-        microsoft-maccatalyst-sdk-full \
-        microsoft-macos-sdk-full \
-        microsoft-tvos-sdk-full \
-        maui \
-
-sudo dotnet workload update
-
-
-sudo \
-    dotnet workload uninstall \
-        maui
-
-sudo \
-    dotnet workload install \
-        maui
