3862b5705b90fa335a64a6ce1d2f82ea79719bff
moljac <mcvjetko@holisticware.net>
2022-07-17 16:35:30 +0200

    cleaning refactored

---
+++ 01-system-integration/mac/01-uninstall-clean-remove-nuke/all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ./nuget.sh
+source ./visual-studio.sh
+source ./xamarin.sh
+source ./dotnet.sh
---
+++ 01-system-integration/mac/01-uninstall-clean-remove-nuke/dotnet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://github.com/dotnet/sdk/blob/main/scripts/obtain/uninstall/dotnet-uninstall-pkgs.sh
+
+sudo rm -rf /usr/local/share/dotnet/
+sudo rm -fr /etc/paths.d/dotnet  
+
+rm -fr "$HOME/.dotnet/"
+
+rm -fr "$HOME/Library/Caches/dotnet-script/"
+
+
