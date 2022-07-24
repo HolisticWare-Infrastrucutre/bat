4d607bc729656d0b2bf7f627ec5c7f055daacf3e
moljac <mcvjetko@holisticware.net>
2022-07-24 21:30:21 +0200

    Update install-prerequisites.sh

--- 02-development/mobile/xamarin.android/install-prerequisites.sh
+++ 02-development/mobile/xamarin.android/install-prerequisites.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+
+# https://github.com/xamarin/xamarin-android/wiki/How-to-Contribute-Code
+
+brew install \
+    automake \
+    cmake \
+    ccache \
+    libtool \
+    p7zip \
+    gdk-pixbuf \
+    gettext \
+    coreutils \
+    findutils \
+    gnu-tar \
+    gnu-sed \
+    gawk \
+    gnutls \
+    gnu-indent \
+    gnu-getopt \
+    intltool \
+    scons \
+    wget \
+    xz \
+
