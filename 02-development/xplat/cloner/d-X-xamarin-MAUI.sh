80f94b90ff46fae03d11b8a9faad8699e239b958
moljac <mcvjetko@holisticware.net>
2022-10-25 10:59:31 +0200

    Update d-X-xamarin-MAUI.sh

--- 02-development/xplat/cloner/d-X-xamarin-MAUI.sh
+++ 02-development/xplat/cloner/d-X-xamarin-MAUI.sh
@@ -28,7 +28,7 @@ export DIR="dotnet/maui/m"
 
 export URL="https://github.com/jsuarezruiz/awesome-dotnet-maui.git"
 export BRANCH="main"
-export DIR="m"
+export DIR="jsuarezruiz--awesome-dotnet-maui"
 [ ! -d $DIR ] \
     && \
     git clone \
@@ -40,7 +40,7 @@ export DIR="m"
 
 export URL="https://github.com/drasticactions/WeatherTwentyTwo.git"
 export BRANCH="main"
-export DIR="m"
+export DIR="drasticactions--WeatherTwentyTwo"
 [ ! -d $DIR ] \
     && \
     git clone \
@@ -52,7 +52,7 @@ export DIR="m"
 
 export URL="https://github.com/davidortinau/WeatherTwentyOne.git"
 export BRANCH="main"
-export DIR="m"
+export DIR="davidortinau--WeatherTwentyOne"
 [ ! -d $DIR ] \
     && \
     git clone \
@@ -64,7 +64,7 @@ export DIR="m"
 
 export URL="https://github.com/dotnet/maui-samples.git"
 export BRANCH="main"
-export DIR="m"
+export DIR="dotnet--maui-samples"
 [ ! -d $DIR ] \
     && \
     git clone \
@@ -76,7 +76,7 @@ export DIR="m"
 
 export URL="https://github.com/rachelkang/recipeSearch.git"
 export BRANCH="main"
-export DIR="m"
+export DIR="rachelkang--recipeSearch"
 [ ! -d $DIR ] \
     && \
     git clone \
@@ -88,7 +88,7 @@ export DIR="m"
 
 export URL="https://github.com/naweed/MauiScientificCalculator.git"
 export BRANCH="main"
-export DIR="m"
+export DIR="naweed--MauiScientificCalculator"
 [ ! -d $DIR ] \
     && \
     git clone \
@@ -100,19 +100,7 @@ export DIR="m"
 
 export URL="https://github.com/jsuarezruiz/dotnet-maui-samples.git"
 export BRANCH="main"
-export DIR="m"
-[ ! -d $DIR ] \
-    && \
-    git clone \
-        --recurse-submodules \
-        -j8 \
-        --branch $BRANCH \
-        $URL \
-        $DIR
-
-export URL="https://github.com/brminnick/HackerNews.git"
-export BRANCH="main"
-export DIR="m"
+export DIR="jsuarezruiz--dotnet-maui-samples"
 [ ! -d $DIR ] \
     && \
     git clone \
@@ -124,7 +112,7 @@ export DIR="m"
 
 export URL="https://github.com/microsoft/dotnet-podcasts.git"
 export BRANCH="main"
-export DIR="m"
+export DIR="microsoft--dotnet-podcasts"
 [ ! -d $DIR ] \
     && \
     git clone \
@@ -136,7 +124,7 @@ export DIR="m"
 
 export URL="https://github.com/dotnet-presentations/dotnet-maui-workshop.git"
 export BRANCH="main"
-export DIR="m"
+export DIR="dotnet-presentations--dotnet-maui-workshop"
 [ ! -d $DIR ] \
     && \
     git clone \
@@ -147,7 +135,7 @@ export DIR="m"
         $DIR
 
 export URL="https://github.com/StoicDreams/BlazorUI.git"
-export BRANCH="main"
+export BRANCH="StoicDreams--BlazorUI"
 export DIR="m"
 [ ! -d $DIR ] \
     && \
@@ -160,7 +148,7 @@ export DIR="m"
 
 export URL="https://github.com/BlazorComponent/MASA.Blazor.git"
 export BRANCH="main"
-export DIR="m"
+export DIR="BlazorComponent--MASA.Blazor"
 [ ! -d $DIR ] \
     && \
     git clone \
@@ -171,6 +159,29 @@ export DIR="m"
         $DIR
 
 
+export URL="https://github.com/brminnick/HackerNews.git"
+export BRANCH="main"
+export DIR="HackerNews//brminnick--HackerNews"
+[ ! -d $DIR ] \
+    && \
+    git clone \
+        --recurse-submodules \
+        -j8 \
+        --branch $BRANCH \
+        $URL \
+        $DIR
+
+export URL="https://github.com/J-Swift/Noctal.git"
+export BRANCH="main"
+export DIR="HackerNews/J-Swift--Noctal"
+[ ! -d $DIR ] \
+    && \
+    git clone \
+        --recurse-submodules \
+        -j8 \
+        --branch $BRANCH \
+        $URL \
+        $DIR
 
 
 
