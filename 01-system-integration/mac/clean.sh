9f2d48424dfad96d08ccc972790714191ca852d8
moljac <mcvjetko@holisticware.net>
2023-02-01 12:46:58 +0100

    Update clean.sh

--- 01-system-integration/mac/clean.sh
+++ 01-system-integration/mac/clean.sh
@@ -231,43 +231,11 @@ rm -fr $HOME/.local/share/Xamarin/
 rm -fr $HOME/.local/share/XamarinInsights/Xamarin\ Android\ Player/
 rm -fr $HOME/.local/share/XamarinInsights/XamarinStudio/
 
-
-
 rm -fr $HOME/.Bytecode-Viewer/
 # rm -fr $HOME/octave/
 rm -fr $HOME/android-archives/
 
 df >> df-post.txt
 
-nuget update -self
-
-# Clear the 3.x+ cache (use either command)
-dotnet nuget locals http-cache --clear
-nuget locals http-cache -clear
-
-# Clear the 2.x cache (NuGet CLI 3.5 and earlier only)
-nuget locals packages-cache -clear
-
-# Clear the global packages folder (use either command)
-dotnet nuget locals global-packages --clear
-nuget locals global-packages -clear
-
-# Clear the temporary cache (use either command)
-dotnet nuget locals temp --clear
-nuget locals temp -clear
-
-# Clear the plugins cache (use either command)
-dotnet nuget locals plugins-cache --clear
-nuget locals plugins-cache -clear
-
-# Clear all caches (use either command)
-dotnet nuget locals all --clear
-nuget locals all -clear
-
-dotnet nuget locals all --clear
-dotnet nuget locals global-packages -c
-dotnet nuget locals temp -c
-
-
-# dotnet nuget locals all -c
-dotnet nuget locals all --clear
+source ./clean-nuget.sh
+source ./clean-android-gradle.sh
