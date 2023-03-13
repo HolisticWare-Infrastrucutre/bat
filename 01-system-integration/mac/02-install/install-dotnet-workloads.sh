7702accacc886bcbfa3ea219b977acffc2d992db
moljac <mcvjetko@holisticware.net>
2023-03-13 10:22:44 +0100

    Update install-dotnet-workloads.sh

--- 01-system-integration/mac/02-install/install-dotnet-workloads.sh
+++ 01-system-integration/mac/02-install/install-dotnet-workloads.sh
@@ -41,8 +41,21 @@ sudo \
             maui-windows \
             tvos \
             wasm-tools \
-                --from-rollback-file https://aka.ms/dotnet/maui/main.json \
+                --source https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-runtime-dea52219/nuget/v3/index.json \
                 --source https://aka.ms/dotnet6/nuget/index.json \
-                --source https://aka.ms/dotnet/maui/main.json \
-                --source https://api.nuget.org/v3/index.json
+                --source https://api.nuget.org/v3/index.json \
 
+
+
+
+sudo dotnet workload \
+    install \
+        maui \
+        maui-android \
+        maui-ios \
+        maui-maccatalyst \
+        maui-desktop \
+        maui-mobile \
+        maui-windows \
+        maui-tizen \
+        wasm-tools-net6 \
