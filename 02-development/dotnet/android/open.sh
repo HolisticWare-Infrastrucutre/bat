7b782806c767785e51c24a06e97064eb2d516c3b
moljac <mcvjetko@holisticware.net>
2023-12-24 17:06:20 +0100

    Create open.sh

---
+++ 02-development/dotnet/android/open.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+
+https://github.com/dotnet/maui
+https://github.com/dotnet/maui/issues
+https://github.com/dotnet/maui/pulls
+https://github.com/dotnet/maui/wiki/DeviceTests
+https://github.com/dotnet/maui/wiki/UITests
+https://github.com/moljac/HolisticWare.WebSite.Notes/blob/master/dotnet-netfx/dotnet/ui-user-interface/maui-multi-platform-app-ui/testing.md
