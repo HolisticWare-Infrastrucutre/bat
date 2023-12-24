5896f2210214cbeaa3ca8c4f5fa9eaa4e90bc593
moljac <mcvjetko@holisticware.net>
2023-12-24 17:06:23 +0100

    Create open.sh

---
+++ 02-development/dotnet/maui/open.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# https://peter.sh/experiments/chromium-command-line-switches/
+
+open -an \
+    "Google Chrome" \
+    --args \
+        --new-window \
+            https://github.com/xamarin/AndroidX \
+            https://github.com/xamarin/GooglePlayServicesComponents \
+            https://github.com/xamarin/AndroidX/pulls \
+            https://github.com/xamarin/AndroidX/issues \
+            https://github.com/xamarin/GooglePlayServicesComponents/pulls \
+            https://github.com/xamarin/GooglePlayServicesComponents/issues \
+            https://github.com/xamarin/xamarin-android \
+            https://github.com/xamarin/java.interop \
+            https://github.com/orgs/xamarin/repositories \
+
+
+
