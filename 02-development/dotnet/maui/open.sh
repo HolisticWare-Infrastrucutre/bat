1f90967357d27db0c198bda7be319d3c0e2bdc1a
moljac <mcvjetko@holisticware.net>
2023-12-27 09:49:34 +0100

    Update open.sh

--- 02-development/dotnet/maui/open.sh
+++ 02-development/dotnet/maui/open.sh
@@ -2,19 +2,19 @@
 
 # https://peter.sh/experiments/chromium-command-line-switches/
 
-open -an \
+# open in
+#   new window
+#   multiple urls in tabs
+open -n -a \
     "Google Chrome" \
-    --args \
-        --new-window \
-            https://github.com/xamarin/AndroidX \
-            https://github.com/xamarin/GooglePlayServicesComponents \
-            https://github.com/xamarin/AndroidX/pulls \
-            https://github.com/xamarin/AndroidX/issues \
-            https://github.com/xamarin/GooglePlayServicesComponents/pulls \
-            https://github.com/xamarin/GooglePlayServicesComponents/issues \
-            https://github.com/xamarin/xamarin-android \
-            https://github.com/xamarin/java.interop \
-            https://github.com/orgs/xamarin/repositories \
-
+        --args \
+            --new-window \
+        https://github.com/dotnet/maui \
+        https://github.com/dotnet/maui/issues \
+        https://github.com/dotnet/maui/pulls \
+        https://github.com/dotnet/maui/wiki/DeviceTests \
+        https://github.com/dotnet/maui/wiki/UITests \
+        https://github.com/moljac/HolisticWare.WebSite.Notes/blob/master/dotnet-netfx/dotnet/ui-user-interface/maui-multi-platform-app-ui/testing.md \
+        
 
 
