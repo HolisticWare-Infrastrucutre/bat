d32f6120b0dca67900f4e92c9ed0e3a527e088b6
moljac <mcvjetko@holisticware.net>
2023-12-27 09:55:44 +0100

    Create open.sh

---
+++ 03-productivity/sports/judo/open.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# open in
+#   new window
+#   multiple urls in tabs
+open -n -a \
+    "Google Chrome" \
+        --args \
+            --new-window \
+                https://judo.hr \
+                https://ijf.org \
+                https://www.eju.net/ \
+                https://www.timeanddate.com/date/dateadd.html \
