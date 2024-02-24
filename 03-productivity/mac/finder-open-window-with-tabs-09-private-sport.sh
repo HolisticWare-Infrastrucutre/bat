36a0fca161f281fbe7c5fadd57e5d17fd28625ff
moljac <mcvjetko@holisticware.net>
2024-02-24 15:45:20 +0100

    scpts files

---
+++ 03-productivity/mac/finder-open-window-with-tabs-09-private-sport.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+export PREFIX="$HOME/bat/03-productivity/mac/"
+
+osascript \
+    $PREFIX/finder-open-window-with-tabs-09-private-sport.scpt
+
+term \
+  /Volumes/xFAT-1TB-2/e/personal-private/judo/HJS/01-pravno/DORH/02-kaznena-prijava-koraci/TODO/mails
