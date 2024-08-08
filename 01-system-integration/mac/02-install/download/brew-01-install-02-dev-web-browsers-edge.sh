10485c2d4b43ce8c49e93024761bbd4ea0705d73
moljac <mcvjetko@holisticware.net>
2024-08-08 23:29:38 +0200

    Create brew-01-install-02-dev-web-browsers-edge.sh

---
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-web-browsers-edge.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+brew uninstall --cask \
+    microsoft-edge \
+    microsoft-edge@beta \
+    microsoft-edge@dev \
+
+
+
+    brew install --cask \
+    microsoft-edge \
+    microsoft-edge@beta \
+    microsoft-edge@dev \
