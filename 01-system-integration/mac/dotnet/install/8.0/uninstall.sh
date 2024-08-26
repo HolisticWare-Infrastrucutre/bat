68745d7893a46f14d054d289fbd8cde6e1640aad
moljac <mcvjetko@holisticware.net>
2024-08-26 21:01:45 +0200

    net8.0

---
+++ 01-system-integration/mac/dotnet/install/8.0/uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo rm -fr /usr/local/share/dotnet/sdk/8.0.*
+
+sudo rm -fr /usr/local/share/dotnet/shared/Microsoft.NETCore.App/8.0.*
+sudo rm -fr /usr/local/share/dotnet/shared/Microsoft.AspNetCore.App/8.0.*
+
