63f2e0746199e278ef983e70d240e0ae520fdefd
moljac <mcvjetko@holisticware.net>
2024-08-26 21:01:57 +0200

    net9.0

---
+++ 01-system-integration/mac/dotnet/install/9.0/uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo rm -fr /usr/local/share/dotnet/sdk/9.0.*
+
+sudo rm -fr /usr/local/share/dotnet/shared/Microsoft.NETCore.App/9.0.*
+sudo rm -fr /usr/local/share/dotnet/shared/Microsoft.AspNetCore.App/9.0.*
+
