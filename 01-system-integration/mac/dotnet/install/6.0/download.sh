b3e0c657346c9682485b2bfaa1b2137d7a8e749c
moljac <mcvjetko@holisticware.net>
2024-08-31 22:26:13 +0200

    Create download.sh

---
+++ 01-system-integration/mac/dotnet/install/6.0/download.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# https://dotnet.microsoft.com/en-us/download/dotnet
+# https://github.com/dotnet/installer/issues/11040
+
+export OS=osx
+export EXT=pkg
+
+export VERSIONS=\
+"
+6.0
+6.0.1xx
+6.0.4xx
+"
+export PRODUCTS=\
+"
+sdk
+runtime
+"
+
+
+if [[ $(uname -m) == 'arm64' ]]; 
+then
+  echo ARM M1 - $(uname -m)
+  export ARCH=arm64
+else
+  echo Intel x86 - $(uname -m)
+  export ARCH=x86
+fi
+
+mkdir $HOME/Downloads/dotnet/
+
+IFS=$'\n'
+# ZSH does not split words by default (like other shells):
+setopt sh_word_split
+
+for VERSION in $VERSIONS
+do
+    if [[ $VERSION == "#"* ]]
+    then
+        continue
+    fi
+
+    mkdir $HOME/Downloads/dotnet/$VERSION/
+    mkdir $HOME/Downloads/dotnet/$VERSION/$OS/
+    mkdir $HOME/Downloads/dotnet/$VERSION/$OS/$ARCH/
+    mkdir $HOME/Downloads/dotnet/$VERSION/$OS/$ARCH/daily/
+    mkdir $HOME/Downloads/dotnet/$VERSION/$OS/$ARCH/preview/
+
+
+    for PRODUCT in $PRODUCTS
+    do
+        if [[ $PRODUCT == "#"* ]]
+        then
+            continue
+        fi
+
+        echo PRODUCT: $PRODUCT
+        echo ARCH: $ARCH
+
+        curl \
+            -v -L -C - \
+            --output-dir $HOME/Downloads/dotnet/$VERSION/$OS/$ARCH/ \
+        -O https://aka.ms/dotnet/$VERSION/dotnet-$PRODUCT-$OS-$ARCH.$EXT
+
+        curl \
+            -v -L -C - \
+            --output-dir $HOME/Downloads/dotnet/$VERSION/$OS/$ARCH/daily/ \
+        -O https://aka.ms/dotnet/$VERSION/daily/dotnet-$PRODUCT-$OS-$ARCH.$EXT
+
+        curl \
+            -v -L -C - \
+            --output-dir $HOME/Downloads/dotnet/$VERSION/$OS/$ARCH/preview/ \
+        -O https://aka.ms/dotnet/$VERSION/preview/dotnet-$PRODUCT-$OS-$ARCH.$EXT
+
+    done
+
+done
