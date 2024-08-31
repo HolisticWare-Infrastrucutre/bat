1382ca97dad4dd75b6dae30d285efeb46873efe0
moljac <mcvjetko@holisticware.net>
2024-08-31 22:26:18 +0200

    Update install.sh

--- 01-system-integration/mac/dotnet/install/6.0/install.sh
+++ 01-system-integration/mac/dotnet/install/6.0/install.sh
@@ -3,69 +3,29 @@
 # https://dotnet.microsoft.com/en-us/download/dotnet
 # https://github.com/dotnet/installer/issues/11040
 
+
+# export PACKAGES=$(ls -1 $HOME/Downloads/dotnet/$VERSION/$OS/$ARCH/*.pkg)
+
 export OS=osx
-export EXT=pkg
+export VERSION=6.0.4xx
 
-export VERSIONS=\
-"
-6.0.1xx
-6.0.4xx
-"
-export PRODUCTS=\
-"
-sdk
-runtime
-"
-export ARCHITECTURES=\
-"
-arm64
-x86
-"
 
-mkdir $HOME/Downloads/dotnet/
+if [[ $(uname -m) == 'arm64' ]]; 
+then
+  echo ARM M1 - $(uname -m)
+  export ARCH=arm64
+else
+  echo Intel x86 - $(uname -m)
+  export ARCH=x86
+fi
+
 
 IFS=$'\n'
 # ZSH does not split words by default (like other shells):
 setopt sh_word_split
 
-for VERSION in $VERSIONS
+for PACKAGE in $HOME/Downloads/dotnet/$VERSION/$OS/$ARCH/*.pkg
 do
-    if [[ $VERSION == "#"* ]]
-    then
-        continue
-    fi
-
-    mkdir $HOME/Downloads/dotnet/$VERSION/
-    mkdir $HOME/Downloads/dotnet/$VERSION/$OS/
-
-    for ARCH in $ARCHITECTURES
-    do
-        if [[ $ARCH == "#"* ]]
-        then
-            continue
-        fi
-
-        mkdir $HOME/Downloads/dotnet/$VERSION/$OS/$ARCH/
-
-        for PRODUCT in $PRODUCTS
-        do
-            if [[ $PRODUCT == "#"* ]]
-            then
-                continue
-            fi
-
-            echo PRODUCT: $PRODUCT
-            echo ARCH: $ARCH
-
-            curl \
-                -v -L -C - \
-                --output-dir $HOME/Downloads/dotnet/$VERSION/$OS/$ARCH/ \
-            -O https://aka.ms/dotnet/$VERSION/dotnet-$PRODUCT-$OS-$ARCH.$EXT
-
-        done
-    done
-
+    sudo installer -verbose -pkg "$PACKAGE" -target /
+    # installer -pkg myapp.pkg -target CurrentUserHomeDirectory
 done
-
-
-
