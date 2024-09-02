bbf6ee942a1516122c32018e1fbf35601ca39c54
moljac <mcvjetko@holisticware.net>
2024-09-02 14:18:16 +0200

    Create nuke.sh

---
+++ 01-system-integration/mac/dotnet/nuke.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+sudo rm -fr \
+    /usr/local/share/dotnet/sdk/* \
+    /usr/local/share/dotnet/sdk-manifests/* \
+    /usr/local/share/dotnet/shared/Microsoft.AspNetCore.App/* \
+    /usr/local/share/dotnet/shared/Microsoft.NETCore.App/* \
