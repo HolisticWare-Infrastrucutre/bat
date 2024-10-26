6d60df678fbc2bfef708a2b971c138b5f2e984be
moljac <mcvjetko@holisticware.net>
2024-10-26 23:41:41 +0200

    Create brew-01-install-02-dev-mobile-ios.sh

---
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-mobile-ios.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Action/Verb
+# install / uninstall / reinstall
+export ACTION_VERB=install
+
+
+brew  doctor
+brew  cleanup  
+
+# Install the Command Line Tools
+xcode-select --install
+
+#----------------------------------------------------------------------------------------------
+# tools development
+brew $ACTION_VERB \
+    cocoapods \
+
+brew $ACTION_VERB \
+    fastlane
+# https://github.com/fastlane/snapshot
+snapshot reset_simulator
+#----------------------------------------------------------------------------------------------
