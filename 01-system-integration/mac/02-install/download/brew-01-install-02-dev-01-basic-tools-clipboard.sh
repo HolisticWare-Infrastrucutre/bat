e448e2980e575bb41664af1c5ce5a9380ba49d26
moljac <mcvjetko@holisticware.net>
2024-11-05 07:52:18 +0100

    Create brew-01-install-02-dev-01-basic-tools-clipboard.sh

---
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-01-basic-tools-clipboard.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Action/Verb
+# install / uninstall / reinstall
+export ACTION_VERB=install
+
+#-----------------------------------------------------------------------------------------------------------------------
+# clipboard
+
+# https://www.reddit.com/r/macapps/comments/13xg34c/best_clipboard_managers_for_macos_working_on/
+# https://www.reddit.com/r/macapps/comments/15i6n0y/whats_the_best_clipboard_app_for_mac/
+# https://www.reddit.com/r/mac/comments/1alywep/clipboard_history/
+# https://www.reddit.com/r/macapps/comments/1cle6xz/battle_of_the_clipboard_managers/
+# https://www.reddit.com/r/macapps/comments/1cle6xz/battle_of_the_clipboard_managers/
+
+# https://github.com/p0deje/Maccy
+
+brew install \
+    maccy
+
+brew install --cask \
+    copyclip
+    
