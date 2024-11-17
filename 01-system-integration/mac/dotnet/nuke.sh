aa2a1b03af7216c4342e931b526b8aa537def394
moljac <mcvjetko@holisticware.net>
2024-11-17 22:39:09 +0100

    Update nuke.sh

--- 01-system-integration/mac/dotnet/nuke.sh
+++ 01-system-integration/mac/dotnet/nuke.sh
@@ -1,7 +1,14 @@
 #!/bin/zsh
 
-sudo rm -fr \
-    /usr/local/share/dotnet/sdk/* \
-    /usr/local/share/dotnet/sdk-manifests/* \
-    /usr/local/share/dotnet/shared/Microsoft.AspNetCore.App/* \
-    /usr/local/share/dotnet/shared/Microsoft.NETCore.App/* \
+  echo "---------------------------------------------------------------------------------------------------------------"
+  echo \
+  "
+  sudo rm -fr \\
+    /usr/local/share/dotnet/ \\
+    $HOME/.dotnet/
+  "
+
+  sudo rm -fr \
+    /usr/local/share/dotnet/ \
+    $HOME/.dotnet/
+
