0c4a966ef647887d81be1c35436a0570e774e68a
moljac <mcvjetko@holisticware.net>
2024-11-17 22:39:06 +0100

    Create nuke-sdks.sh

---
+++ 01-system-integration/mac/dotnet/nuke-sdks.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+  echo "--------------------------------------------------------------------------------------------"
+  echo \
+  "
+  sudo rm -fr \
+      /usr/local/share/dotnet/sdk/* \
+      /usr/local/share/dotnet/sdk-manifests/* \
+      /usr/local/share/dotnet/shared/Microsoft.AspNetCore.App/* \
+      /usr/local/share/dotnet/shared/Microsoft.NETCore.App/* \
+  "
+
+sudo rm -fr \
+    /usr/local/share/dotnet/sdk/* \
+    /usr/local/share/dotnet/sdk-manifests/* \
+    /usr/local/share/dotnet/shared/Microsoft.AspNetCore.App/* \
+    /usr/local/share/dotnet/shared/Microsoft.NETCore.App/* \
+
+  echo "--------------------------------------------------------------------------------------------"
