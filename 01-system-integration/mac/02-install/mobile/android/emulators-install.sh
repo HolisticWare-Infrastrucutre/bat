4ca1b664da34fb18996c7efcc9b17c4535e5c3c9
moljac <mcvjetko@holisticware.net>
2025-02-24 21:54:58 +0100

    Update emulators-install.sh

--- 01-system-integration/mac/02-install/mobile/android/emulators-install.sh
+++ 01-system-integration/mac/02-install/mobile/android/emulators-install.sh
@@ -5,50 +5,80 @@
 # "$(dirname "${BASH_SOURCE[0]}")/emulators-data-names.sh"
 source "$(dirname "$0")/emulators-data-names.sh"
 
-# java 8 is required
-export JAVA_HOME=$JAVA_HOME_JDK_8
+
+export JAVA_HOME=$JAVA_HOME_JDK_21
 java -version
 
-echo \
-"
-    echo "no" \\
-        | \\
-        avdmanager \\
-            --verbose \\
-                create avd \\
-                --force \\
-                --name $EMULATOR_NAME_PREFIX-$EMULATOR \\
-                --device "$EMULATOR" \\
-                --package "system-images\;android-34\;google_apis\;arm64-v8a" \\
-                --tag "google_apis" \\
-                --abi "arm64-v8a" \\
+echo EMULATOR_NAME_PREFIX_34=$EMULATOR_NAME_PREFIX_34
+echo EMULATOR_NAME_PREFIX_35=$EMULATOR_NAME_PREFIX_35
 
-"
+export AVDMANAGER=$HOME/Library/Android/sdk/cmdline-tools/11.0/bin/avdmanager
 
 IFS=$'\n'
 # ZSH does not split words by default (like other shells):
 setopt sh_word_split
 
-for EMULATOR in $EMULATORS;
+for DEVICE in $AVD_DEVICES;
 do
     echo "=============================================================================================================="
-    echo "AVD: $EMULATOR"
+    echo "AVD"
+    echo "      DEVICE: $DEVICE"
+
+    if [[ $DEVICE == *"Tablet"* ]]; 
+    then
+        continue
+    else
+    fi
+
+    echo \
+    "
+        $AVDMANAGER \\
+            --verbose \\
+                create avd \\
+                --force \\
+                --name $EMULATOR_NAME_PREFIX_34-$EMULATOR \\
+                --device \"$DEVICE\" \\
+                --package \"system-images;arm64-v8a;google_apis;android-34\" \\
+                --tag \"google_apis\" \\
+                --abi \"arm64-v8a\" \\
+                    
+        $AVDMANAGER \\
+            --verbose \\
+                create avd \\
+                --force \\
+                --name $EMULATOR_NAME_PREFIX_35-$EMULATOR \\
+                --device \"$DEVICE\" \\
+                --package \"system-images;arm64-v8a;google_apis;android-35\" \\
+                --tag \"google_apis\" \\
+                --abi \"arm64-v8a\" \\
+    "
 
     echo "no" \
         | \
-        avdmanager \
+        $AVDMANAGER \
             --verbose \
                 create avd \
                 --force \
-                --name $EMULATOR_NAME_PREFIX-$EMULATOR \
-                --device "$EMULATOR" \
+                --name $EMULATOR_NAME_PREFIX_34-$EMULATOR \
+                --device "$DEVICE" \
                 --package "system-images;android-34;google_apis;arm64-v8a" \
                 --tag "google_apis" \
                 --abi "arm64-v8a" \
 
-done
+    echo "no" \
+        | \
+        $AVDMANAGER \
+            --verbose \
+                create avd \
+                --force \
+                --name $EMULATOR_NAME_PREFIX_35-$EMULATOR \
+                --device "$DEVICE" \
+                --package "system-images;android-35;google_apis;arm64-v8a" \
+                --tag "google_apis" \
+                --abi "arm64-v8a" \
 
-export EMULATOR_NAME_PREFIX="emulator-android-34-google-apis-arm-v8a"
+
+done
 
 echo "no" \
     | \
@@ -70,3 +100,21 @@ do
     dev_android_emulator_launch $EMULATOR_NAME_PREFIX-$EMULATOR
 done
 
+
+
+
+    echo "no" \
+        | \
+        $AVDMANAGER \
+                create avd \
+                --force \
+                --name emulator--arm-v8a-google-apis-android-35-pixel_c \
+                --device "pixel_c" \
+                --package "system-images;android-35;google_apis;arm64-v8a" \
+                --tag "google_apis" \
+                --abi "arm64-v8a" \
+
+$AVDMANAGER \
+    list avd
+
+ll $HOME/.android/avd     
