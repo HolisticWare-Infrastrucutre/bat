1368566498c10bbfa17a768ce5da8d5caf26d08e
moljac <mcvjetko@holisticware.net>
2025-03-05 17:04:14 +0100

    Update brew-01-install-02-dev-01-basic-editors.sh

--- 01-system-integration/mac/02-install/download/brew-01-install-02-dev-01-basic-editors.sh
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-01-basic-editors.sh
@@ -6,16 +6,19 @@ export ACTION_VERB=install
 
 #----------------------------------------------------------------------------------------------
 #
-brew $ACTION_VERB --cask \
+brew $ACTION_VERB \
+    --cask \
     visual-studio-code \
-    homebrew/cask-versions/visual-studio-code-insiders \
-
-#----------------------------------------------------------------------------------------------
-#
-brew $ACTION_VERB --cask \
+    visual-studio-code-insiders \
     sublime-text \
     atom \
     brackets \
     bbedit \
     zed \
     zed@preview \
+    fleet
+
+
+brew $ACTION_VERB \
+    neovim \
+    fleet-cli \
