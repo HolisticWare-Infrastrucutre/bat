ce863e6ead2a49f2700554129d6dcb303b9f38d2
moljac <mcvjetko@holisticware.net>
2025-03-20 15:04:11 +0100

    Create uninstall.sh

---
+++ 01-system-integration/mac/dotnet/workload/uninstall.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+source $HOME/bat/01-system-integration/mac/01-uninstall-clean-remove-nuke/nuget.sh
+
+export WORKLOADZ=\
+"\
+"
+#            maui-tizen \
+
+sudo \
+    dotnet \
+        workload \
+            uninstall \
+                android \
+                android-net8 \
+                ios \
+                maccatalyst \
+                tvos \
+                macos \
+                maui \
+                maui \
+                maui-android \
+                maui-ios \
+                maui-maccatalyst \
+                maui-mobile \
+                maui-windows \
+                maui-tizen \
+                tvos \
+                wasm-tools \
+                wasm-tools-net8 \
+                wasm-tools-net9 \
+                wasm-experimental \
+                wasm-experimental-net8 \
+                wasm-experimental-net9 \
+                aspire \
