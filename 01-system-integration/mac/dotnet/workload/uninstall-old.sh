642de693ded34d1dfc7fb5e0f39c8432b512b9cd
moljac <mcvjetko@holisticware.net>
2025-03-20 15:04:07 +0100

    Create uninstall-old.sh

---
+++ 01-system-integration/mac/dotnet/workload/uninstall-old.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+source $HOME/bat/01-system-integration/mac/01-uninstall-clean-remove-nuke/nuget.sh
+
+export WORKLOADZ=\
+"\
+"
+#            maui-tizen \
+
+sudo \
+    dotnet \
+        workload \
+            uninstall \
+                wasm-tools-net6 \
+                wasm-tools-net7 \
+                wasm-experimental-net7 \
