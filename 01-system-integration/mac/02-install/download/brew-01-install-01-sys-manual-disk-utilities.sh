d6533e95408eb3bc5c029fb6d1b35ca1e605cd1f
moljac <mcvjetko@holisticware.net>
2025-05-13 07:03:10 +0200

    Create brew-01-install-01-sys-manual-disk-utilities.sh

---
+++ 01-system-integration/mac/02-install/download/brew-01-install-01-sys-manual-disk-utilities.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Action/Verb
+# install / uninstall / reinstall
+export ACTION_VERB=install
+
+brew $ACTION_VERB \
+    macfuse \
+    gsmartcontrol \
+    smartmontools \
+
+
+# linux needed
+# ext4fuse
+
