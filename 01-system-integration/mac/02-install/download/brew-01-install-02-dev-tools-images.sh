34e68e86c910cd0a75dfe831a5f14e79ac25154e
moljac <mcvjetko@holisticware.net>
2025-06-16 11:57:46 +0200

    Create brew-01-install-02-dev-tools-images.sh

---
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-tools-images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Action/Verb
+# install / uninstall / reinstall
+export ACTION_VERB=install
+
+
+
+brew $ACTION_VERB \
+    --cask \
+        pngquant \
+        optipng \
+
+
+# optipng -o7 *.edited.zip.png
+
+
