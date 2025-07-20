534423aa3722149ce2fc594bb2dc60ec8964cbee
moljac <mcvjetko@holisticware.net>
2025-07-20 10:10:55 +0200

    Create brew-01-install-02-dev-01-basic-security.sh

---
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-01-basic-security.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Action/Verb
+# install / uninstall / reinstall
+export ACTION_VERB=install
+
+brew doctor
+brew upgrade
+brew update
+
+#----------------------------------------------------------------------------------------------
+#
+brew $ACTION_VERB \
+    --cask \
+        keepassxc
