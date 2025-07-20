15fd1d5b360fb83926d36d413b775c7f4cdcd61d
moljac <mcvjetko@holisticware.net>
2025-07-20 10:11:03 +0200

    Update brew-01-install-02-dev-01-basic-tools-menu-bar-aka-notch.sh

--- 01-system-integration/mac/02-install/download/brew-01-install-02-dev-01-basic-tools-menu-bar-aka-notch.sh
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-01-basic-tools-menu-bar-aka-notch.sh
@@ -4,16 +4,23 @@
 # install / uninstall / reinstall
 export ACTION_VERB=install
 
+brew doctor
+brew upgrade
+brew update
+
 #----------------------------------------------------------------------------------------------
 # menu bar aka notch
 
 # https://www.jessesquires.com/blog/2023/12/16/macbook-notch-and-menu-bar-fixes/
 # https://github.com/dwarvesf/hidden
-brew install --cask \
-    hiddenbar
+brew $ACTION_VERB \
+    --cask \
+        hiddenbar
 
 open -a "Hidden Bar"
 
 # https://github.com/zkondor/znotch
 brew tap zkondor/dist
-brew install --cask znotch
+brew $ACTION_VERB \
+    --cask \
+        znotch
