3efea9f18ef968b6d7a01eb2c5c7c87a83a49726
moljac <mcvjetko@holisticware.net>
2025-07-21 07:39:29 +0200

    Update brew-01-install-03-sys-virtualization.sh

--- 01-system-integration/mac/02-install/download/brew-01-install-03-sys-virtualization.sh
+++ 01-system-integration/mac/02-install/download/brew-01-install-03-sys-virtualization.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# Action/Verb
+# install / uninstall / reinstall
+export ACTION_VERB=install
 
+brew doctor
+brew upgrade
+brew update
 
+#----------------------------------------------------------------------------------------------
 sudo \
-    brew $ACTION_VERB --cask \
-        parallels \
+    brew $ACTION_VERB \
+        --cask \
+            parallels \
+            virtualbox \
+            virtualbuddy \
+            virtualbuddy@beta \
