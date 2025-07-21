4502b7cb36a8a2defc815636c27fee73f13f4f2f
moljac <mcvjetko@holisticware.net>
2025-07-21 07:39:26 +0200

    Update brew-01-install-02-dev-web-browsers.sh

--- 01-system-integration/mac/02-install/download/brew-01-install-02-dev-web-browsers.sh
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-web-browsers.sh
@@ -4,77 +4,66 @@
 # install / uninstall / reinstall
 export ACTION_VERB=install
 
+brew doctor
+brew upgrade
+brew update
+
 #----------------------------------------------------------------------------------------------
 # browsers
-brew tap homebrew/cask-versions
-
-brew $ACTION_VERB --cask \
-    google-chrome \
-    google-chrome-canary \
-    google-chrome-beta \
-    google-chrome-dev \
-    microsoft-edge \
-    microsoft-edge-beta \
-    firefox \
-    firefox-developer-edition \
-    firefox-nightly \
-    opera \
-    opera-beta \
-    opera-developer \
-    chromium \
-    tor-browser \
-    opera \
-    opera-neon \
 
 brew $ACTION_VERB \
-    --force \
     --cask \
-        google-chrome@beta \
+        google-chrome \
         google-chrome@canary \
-        google-chrome@dev
+        google-chrome@beta \
+        google-chrome@dev \
+        microsoft-edge \
+        microsoft-edge@beta \
+        microsoft-edge@dev \
+        firefox \
+        firefox@developer-edition \
+        firefox@nightly \
+        opera \
+        opera@beta \
+        opera-developer \
+        chromium \
+        tor-browser \
+        opera \
+        opera-neon \
+        brave-browser \
+        brave-browser@beta \
+        tor-browser \
+        duckduckgo \
 
-
-brew $ACTION_VERB --cask \
-    browserosaurus \
-    charles \
-    chromedriver \
-    opera-mobile-emulator \
-
-brew $ACTION_VERB --cask \
-    microsoft-edge \
-    microsoft-edge-dev \
-    microsoft-edge-beta \
-    google-chrome \
-    google-chrome-canary \
-    firefox \
-    firefox-developer-edition \
-    brave-browser \
-    brave-browser@dev \
-    brave-browser@beta \
-
-
-#   conflicts
-brew $ACTION_VERB --cask \
-    tor-browser-alpha \
-    firefox-beta \
+#       tor-browser@alpha \
+#       brave-browser@dev \
 
 brew $ACTION_VERB \
+    --cask \
+        browserosaurus \
+        charles \
+        chromedriver \
+        opera-mobile-emulator \
+
+brew $ACTION_VERB \
+    --cask \
+        postman \
+        postman-agent \
+        postman-cli \
+        postmancanary \
+        protonvpn \
+
+brew $ACTION_VERB \
+    --formula \
     tor
-
-brew untap homebrew/cask-versions
-
-brew $ACTION_VERB --cask \
-    postman \
-    postman-agent \
-    postman-cli \
-    postmancanary \
-    protonvpn \
-
-brew $ACTION_VERB --cask \
-    duckduckgo
 
 # https://github.com/jarun/ddgr
 # duck duck go 
 brew $ACTION_VERB \
+    --formula \
     ddgr
+
+brew untap homebrew/cask-versions
+
+
 
