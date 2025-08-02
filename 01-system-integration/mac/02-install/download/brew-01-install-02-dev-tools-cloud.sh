b3a6faf5e397f56bcc635025669192671a91d128
moljac <mcvjetko@holisticware.net>
2025-08-02 09:39:46 +0200

    Update brew-01-install-02-dev-tools-cloud.sh

--- 01-system-integration/mac/02-install/download/brew-01-install-02-dev-tools-cloud.sh
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-tools-cloud.sh
@@ -6,28 +6,32 @@ export ACTION_VERB=install
 
 
 
-brew uninstall --cask \
-    --force \
-        docker 
-brew uninstall --formula \
-    --force \
-        docker 
+brew uninstall \
+    --cask \
+        --force \
+            docker 
+brew uninstall \
+    --formula \
+        --force \
+            docker 
 
 # DO NOT install forlmula and cask
 # cask is enough
 # https://github.com/Homebrew/homebrew-cask/issues/146078
 #     docker \
 
-brew install --cask \
-    docker
+brew $ACTION_VERB \
+    --cask \
+        docker
 
-brew install \
-    docker-buildx \
-    docker-clean \
-    docker-completion \
-    docker-compose \
-    docker-credential-helper \
-    docker-credential-helper-ecr \
+brew $ACTION_VERB \
+    --formula \
+        docker-buildx \
+        docker-clean \
+        docker-completion \
+        docker-compose \
+        docker-credential-helper \
+        docker-credential-helper-ecr \
 
 mkdir -p ~/.docker/cli-plugins
 ln -sfn \
@@ -37,3 +41,15 @@ mkdir -p $HOME/.docker/cli-plugins
 ln -sfn \
     /opt/homebrew/opt/docker-compose/bin/docker-compose \
     $HOME/.docker/cli-plugins/docker-compose
+
+
+
+brew $ACTION_VERB \
+    --formula \
+        azure-cli \
+
+brew $ACTION_VERB \
+    --cask \
+        gcloud-cli \
+    
+    
