9b9e8cc65c248f5dc8ad1691050e65a9f1a88e11
moljac <mcvjetko@holisticware.net>
2025-08-13 13:27:59 +0200

    Refactor dotnet scripts and expand nuke.sh cleanup
    Moved versioned dotnet scripts and rollback files into a 'versions' subdirectory for better organization. Updated nuke.sh to remove additional user-specific dotnet-related directories, improving cleanup coverage.

--- 01-system-integration/mac/dotnet/nuke.sh
+++ 01-system-integration/mac/dotnet/nuke.sh
@@ -3,12 +3,35 @@
   echo "---------------------------------------------------------------------------------------------------------------"
   echo \
   "
-  sudo rm -fr \\
-    /usr/local/share/dotnet/ \\
-    $HOME/.dotnet/
+    sudo \\
+        rm -fr \\
+            /usr/local/share/dotnet/
+
+    sudo \\
+        rm -fr \\
+        $HOME/.dotnet \\
+        $HOME/.nuget \\
+        $HOME/.aspire \\
+        $HOME/.aspnet \\
+        $HOME/.microsoft/usersecrets/ \\
+
   "
 
-  sudo rm -fr \
-    /usr/local/share/dotnet/ \
-    $HOME/.dotnet/
+  sudo \
+    rm -fr \
+      /usr/local/share/dotnet/ \
+      $HOME/.dotnet/
+
+sudo \
+    rm -fr \
+        /usr/local/share/dotnet/
+
+sudo \
+    rm -fr \
+      $HOME/.dotnet \
+      $HOME/.nuget \
+      $HOME/.aspire \
+      $HOME/.aspnet \
+      $HOME/.microsoft/usersecrets/ \
+
 
