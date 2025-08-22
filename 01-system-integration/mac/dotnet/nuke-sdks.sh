e15f59ab7e6e7184ddbe5b5d40470bfd1b51a676
moljac <mcvjetko@holisticware.net>
2025-08-22 18:01:33 +0200

    Update nuke-sdks.sh

--- 01-system-integration/mac/dotnet/nuke-sdks.sh
+++ 01-system-integration/mac/dotnet/nuke-sdks.sh
@@ -10,10 +10,11 @@
       /usr/local/share/dotnet/shared/Microsoft.NETCore.App/* \
   "
 
-sudo rm -fr \
-    /usr/local/share/dotnet/sdk/* \
-    /usr/local/share/dotnet/sdk-manifests/* \
-    /usr/local/share/dotnet/shared/Microsoft.AspNetCore.App/* \
-    /usr/local/share/dotnet/shared/Microsoft.NETCore.App/* \
+sudo \
+    rm -fr \
+        /usr/local/share/dotnet/sdk/* \
+        /usr/local/share/dotnet/sdk-manifests/* \
+        /usr/local/share/dotnet/shared/Microsoft.AspNetCore.App/* \
+        /usr/local/share/dotnet/shared/Microsoft.NETCore.App/* \
 
   echo "--------------------------------------------------------------------------------------------"
