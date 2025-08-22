ecd8c5439d851165b89d612abce8f3256024b1d1
moljac <mcvjetko@holisticware.net>
2025-08-22 18:01:35 +0200

    Update test.sh

--- 01-system-integration/mac/dotnet/workload/android/test.sh
+++ 01-system-integration/mac/dotnet/workload/android/test.sh
@@ -1,39 +1,53 @@
 #!/bin/zsh
 
-    echo "---------------------------------------------------------------------------------------------------------------"
     echo \
     "
+    ---------------------------------------------------------------------------
+    export JAVA_HOME=$JAVA_HOME_MICROSOFT_17
+    $ANDROID_SDK_ROOT/cmdline-tools/19.0/bin/sdkmanager \
+        --install \
+            "platform-tools"
+    "
+    export JAVA_HOME=$JAVA_HOME_MICROSOFT_17
+    $ANDROID_SDK_ROOT/cmdline-tools/19.0/bin/sdkmanager \
+        --install \
+            "platform-tools"
+
+    echo \
+    "
+    ---------------------------------------------------------------------------
     rm -fr \\
-        $HOME/Downloads/AppAndroid/
+        $HOME/Downloads/dotnet/samples/AppAndroid/
 
     dotnet new \\
         android \\
-            --output $HOME/Downloads/AppAndroid/
+            --output $HOME/Downloads/dotnet/samples/AppAndroid/
 
     "
     rm -fr \
-        $HOME/Downloads/AppAndroid/
+        $HOME/Downloads/dotnet/samples/AppAndroid/
 
     dotnet new \
         android \
-            --output $HOME/Downloads/AppAndroid/
+            --output $HOME/Downloads/dotnet/samples/AppAndroid/
 
     echo \
     "
+    ---------------------------------------------------------------------------
     dotnet \\
         build \\
-        $HOME/Downloads/AppAndroid/ \\
+        $HOME/Downloads/dotnet/samples/AppAndroid/ \\
             -v:diagnostic \\
             -t:InstallAndroidDependencies \\
             -p:AcceptAndroidSdkLicenses=true \\
             -p:AndroidManifestType=GoogleV2 \\
             -p:AndroidSdkDirectory=$ANDROID_SDK_ROOT \\
-            -bl:$HOME/Downloads/AppAndroid/install-android-dependencies-GoogleV2.binlog
+            -bl:$HOME/Downloads/dotnet/samples/AppAndroid/install-android-dependencies-GoogleV2.binlog
 
     "
     dotnet \
         build \
-        $HOME/Downloads/AppAndroid/ \
+        $HOME/Downloads/dotnet/samples/AppAndroid/ \
             -v:diagnostic \
             -t:InstallAndroidDependencies \
             -p:AcceptAndroidSdkLicenses=true \
@@ -41,37 +55,15 @@
             -p:AndroidSdkDirectory=$ANDROID_SDK_ROOT \
             -bl:$HOME/Downloads/AppAndroid/install-android-dependencies-GoogleV2.binlog
 
-
+    echo \
+    "
+    ---------------------------------------------------------------------------
+    dotnet \\
+        run \\
+        --project \\
+            $HOME/Downloads/dotnet/samples/AppAndroid/ \
+    "
     dotnet \
         run \
         --project \
             $HOME/Downloads/AppAndroid/ \
-
-
-
-    dotnet new \
-        uninstall \
-            Microsoft.macOS.Templates
-    dotnet new \
-        install \
-            Microsoft.macOS.Templates
-
-    dotnet new \
-        macos \
-            --output \
-                $HOME/Downloads/AppMacOS/
-
-    dotnet \
-        run \
-        --project \
-                $HOME/Downloads/AppMacOS/
-
-    dotnet new \
-        ios \
-            --output \
-                $HOME/Downloads/AppiOS/
-
-    dotnet \
-        run \
-        --project \
-                $HOME/Downloads/AppiOS/
