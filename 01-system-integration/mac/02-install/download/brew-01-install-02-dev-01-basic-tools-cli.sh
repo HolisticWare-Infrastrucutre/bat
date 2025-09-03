5fbf9be9bbdc77857e073ee3bd984189f8a8935e
moljac <mcvjetko@holisticware.net>
2025-09-03 10:20:03 +0200

    Update brew-01-install-02-dev-01-basic-tools-cli.sh

--- 01-system-integration/mac/02-install/download/brew-01-install-02-dev-01-basic-tools-cli.sh
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-01-basic-tools-cli.sh
@@ -8,41 +8,49 @@ brew doctor
 brew upgrade
 brew update
 
-brew $ACTION_VERB \
-    --formula \
-        git \
-        gh \
-        github-mcp-server \
-        github-markdown-toc \
-        gitlab-ci-local \
-        gitlab-runner \
-        glab \
-        wget \
+brew \
+    $ACTION_VERB \
+        --formula \
+            git \
+            gh \
+            github-mcp-server \
+            github-markdown-toc \
+            gitlab-ci-local \
+            gitlab-runner \
+            glab \
+            wget \
 
 
 
-brew $ACTION_VERB \
-    --cask \
-        github \
-        github@beta \
-        git-credential-manager \
+brew \
+    $ACTION_VERB \
+        --cask \
+            github \
+            github@beta \
+            git-credential-manager \
 
 
 #----------------------------------------------------------------------------------------------
 #
 # find
 # https://github.com/sharkdp/fd
-brew $ACTION_VERB \
-    fd
+brew \
+    $ACTION_VERB \
+        --formula \
+            fd
 
 # grep
 # https://www.warp.dev/
-brew $ACTION_VERB \
-    ripgrep \
+brew \
+    $ACTION_VERB \
+        --formula \
+            ripgrep \
 
 # https://github.com/cli/cli#installation
 # https://cli.github.com/
-brew $ACTION_VERB \
-    gh \
-    glab \
+brew \
+    $ACTION_VERB \
+        --formula \
+            gh \
+            glab \
 
