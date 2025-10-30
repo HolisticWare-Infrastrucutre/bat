f3b9b1433275275f436e62e91ccca3cbcfbefac3
moljac <mcvjetko@holisticware.net>
2025-10-30 12:34:45 +0100

    Update test.sh

--- 01-system-integration/mac/02-install/ai/llama.cpp/android/test.sh
+++ 01-system-integration/mac/02-install/ai/llama.cpp/android/test.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+# https://developer.android.com/training/data-storage
+adb shell <<'EOF'
+cd /data/local/tmp/llama.cpp
+LD_LIBRARY_PATH=./build-android/bin ./build-android/bin/llama-simple -m model.gguf -n 200000 How to deserialize JSON in C#?
+EOF
+
+
