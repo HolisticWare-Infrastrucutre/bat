8ea102dbc4f1f4a13cef7acc8ef0e01abdfe9b61
moljac <mcvjetko@holisticware.net>
2025-10-30 12:34:56 +0100

    Update brew-01-install-02-dev-web-browsers-tools.sh

--- 01-system-integration/mac/02-install/download/brew-01-install-02-dev-web-browsers-tools.sh
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-web-browsers-tools.sh
@@ -11,32 +11,43 @@ brew update
 #----------------------------------------------------------------------------------------------
 # browsers
 
-brew $ACTION_VERB \
-    --cask \
-        browserosaurus \
-        charles \
-        chromedriver \
-        opera-mobile-emulator \
+brew \
+    $ACTION_VERB \
+        --cask \
+            browserosaurus \
+            charles \
+            chromedriver \
+            opera-mobile-emulator \
 
-brew $ACTION_VERB \
-    --cask \
-        postman \
-        postman-agent \
-        postman-cli \
-        postmancanary \
-        protonvpn \
 
-brew $ACTION_VERB \
-    --formula \
-    tor
+brew \
+    $ACTION_VERB \
+        --cask \
+            postman \
+            postman-agent \
+            postman-cli \
+            postmancanary \
+            protonvpn \
+
+brew \
+    $ACTION_VERB \
+        --formula \
+            tor
 
 # https://github.com/jarun/ddgr
 # duck duck go 
-brew $ACTION_VERB \
-    --formula \
-    ddgr
+brew \
+    $ACTION_VERB \
+        --formula \
+            ddgr
 
 brew untap homebrew/cask-versions
 
 
+brew tap AlexStrNik/Browserino
+brew \
+    $ACTION_VERB \
+        --formula \
+        --no-quarantine \
+            browserino
 
