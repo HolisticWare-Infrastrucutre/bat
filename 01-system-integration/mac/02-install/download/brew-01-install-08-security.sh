372006eea6e28b2920d0b552e9e03a660cfaee58
moljac <mcvjetko@holisticware.net>
2025-12-26 16:18:20 +0100

    Create brew-01-install-08-security.sh

---
+++ 01-system-integration/mac/02-install/download/brew-01-install-08-security.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Action/Verb
+# install / uninstall / reinstall
+export ACTION_VERB=install
+
+brew doctor
+brew upgrade
+brew update
+
+
+#----------------------------------------------------------------------------------------------
+#
+brew \
+    $ACTION_VERB \
+        --formula \
+            clamav
+
