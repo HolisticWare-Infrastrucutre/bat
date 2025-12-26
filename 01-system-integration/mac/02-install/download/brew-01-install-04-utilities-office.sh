8701a41ee7bce4588c4cdc19104c90b91a1a964c
moljac <mcvjetko@holisticware.net>
2025-12-26 16:18:17 +0100

    Create brew-01-install-04-utilities-office.sh

---
+++ 01-system-integration/mac/02-install/download/brew-01-install-04-utilities-office.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Action/Verb
+# install / uninstall / reinstall
+export ACTION_VERB=install
+
+brew doctor
+brew upgrade
+brew update
+
+#----------------------------------------------------------------------------------------------
+# communications
+
+# Chat IM
+brew \
+    $ACTION_VERB \
+        --cask \
+            libreoffice \
