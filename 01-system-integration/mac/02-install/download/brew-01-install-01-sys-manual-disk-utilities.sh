dad9e868258e3b8a6839504307adfd2f66313de0
moljac <mcvjetko@holisticware.net>
2026-01-08 13:04:31 +0100

    Update brew-01-install-01-sys-manual-disk-utilities.sh

--- 01-system-integration/mac/02-install/download/brew-01-install-01-sys-manual-disk-utilities.sh
+++ 01-system-integration/mac/02-install/download/brew-01-install-01-sys-manual-disk-utilities.sh
@@ -7,12 +7,17 @@ export ACTION_VERB=install
 
 brew \
     $ACTION_VERB \
-    --formula\
-        macfuse \
-        gsmartcontrol \
-        smartmontools \
+        --formula\
+            macfuse \
+            gsmartcontrol \
+            smartmontools \
 
 
 # linux needed
 # ext4fuse
+
+brew \
+    $ACTION_VERB \
+        --cask \
+            balenaetcher
 
