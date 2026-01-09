f7eac368b6525bb90f2106a44c1a3c14a4cc8d08
moljac <mcvjetko@holisticware.net>
2026-01-09 22:13:45 +0100

    Update create-structure.cs

--- dotnet-csharp/create-structure.cs
+++ dotnet-csharp/create-structure.cs
@@ -2,6 +2,9 @@
 
 #:sdk Microsoft.NET.Sdk
 
+#:package System.CommandLine@2.0.1
+#:package Spectre.Console@0.54.0
+
 using System;
 using System.IO;
 
@@ -9,3 +12,25 @@ using System.IO;
 string base_path = Directory.GetCurrentDirectory();
 
 Console.WriteLine($"Creating project structure in: {base_path}");
+
+string[] directories = new[]
+{
+    "source",
+    "source/business-domain-logic/",
+    "source/user-interface-ui/",
+    "source/utilities/",
+    "tests/",
+    "tests/unit-tests/",
+    "tests/benchmark-tests/",
+    "tests/common/",
+    "docs/",
+    "configs/",
+    "scripts/"
+};
+
+foreach (string dir in directories)
+{
+    string full_path = Path.Combine(base_path, dir);
+    Directory.CreateDirectory(full_path);
+    Console.WriteLine($"Created directory: {full_path}");
+}
