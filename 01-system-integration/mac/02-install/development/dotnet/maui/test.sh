b1b17a5286eff81d9e2b8dbfed1db43cf7d406d2
moljac <mcvjetko@holisticware.net>
2026-01-17 09:24:18 +0100

    Create test.sh

---
+++ 01-system-integration/mac/02-install/development/dotnet/maui/test.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# https://platform.uno/docs/articles/get-started-dotnet-new.html?tabs=macos
+
+echo "---------------------------------------------------------------------------------------------------------------"
+echo \
+"
+"
+
