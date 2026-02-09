4fcb2f57fedabc0dd8502b34e5518fc9848658fa
moljac <mcvjetko@holisticware.net>
2026-02-09 21:05:32 +0100

    Delete test.sh

--- 01-system-integration/mac/02-install/development/dotnet/maui/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# https://platform.uno/docs/articles/get-started-dotnet-new.html?tabs=macos
-
-echo "---------------------------------------------------------------------------------------------------------------"
-echo \
-"
-"
-
