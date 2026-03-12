59f900b7b9de2cca61df5ff199e4279525092635
moljac <mcvjetko@holisticware.net>
2026-03-12 09:24:59 +0100

    Update brew-01-install-02-dev-scientific.sh

--- 01-system-integration/mac/02-install/download/brew-01-install-02-dev-scientific.sh
+++ 01-system-integration/mac/02-install/download/brew-01-install-02-dev-scientific.sh
@@ -4,12 +4,16 @@
 # install / uninstall / reinstall
 export ACTION_VERB=install
 
+brew doctor
+brew upgrade
+brew update
+
+
 #----------------------------------------------------------------------------------------------
 brew tap homebrew/science # adds another source for applications.
 brew $ACTION_VERB \
     octave \
     R \
-    python \
     julia \
 
 #     python3 \
@@ -32,45 +36,4 @@ echo "alias rstudio='open -a RStudio'" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 echo "alias rstudio='open -a RStudio'" >> $HOME/.zshrc
 source $HOME/.zshrc
-
-
-
-#----------------------------------------------------------------------------------------------
-
-python --version
-python3 --version
-which pip
-which pip3
-
-sudo pip3 $ACTION_VERB --upgrade pip
-
-
-# python packages: Scientific computing:
-pip3 $ACTION_VERB numpy
-pip3 $ACTION_VERB scipy
-pip3 $ACTION_VERB sympy
-python -c 'import numpy ; numpy.test();'
-python -c 'import scipy ; scipy.test();'
-
-# python packages: Data management:
-pip3 $ACTION_VERB pandas
-
-
-# python packages: plotting
-pip3 $ACTION_VERB matplotlib
-#brew $ACTION_VERB \
-#    homebrew/python/matplotlib \
-#    --with-cairo \
-#    --with-tex
-
-pip3 $ACTION_VERB --upgrade pip
-pip3 $ACTION_VERB jupyter
-
-pip $ACTION_VERB jupyterlab
-pip $ACTION_VERB notebook
-pip $ACTION_VERB voila
-
-jupyter-lab &
-jupyter notebook &
-voila &
 
