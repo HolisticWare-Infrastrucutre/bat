8bb9d3f5244333b49112e84209f496164216f198
moljac <mcvjetko@holisticware.net>
2026-05-07 16:05:56 +0200

    Create show.sh

---
+++ 03-productivity/mac/topics/ai/ollama/show.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# ollama show -h
+# Show information for a model
+# 
+# Usage:
+#   ollama show MODEL [flags]
+# 
+# Flags:
+#   -h, --help         help for show
+#       --license      Show license of a model
+#       --modelfile    Show Modelfile of a model
+#       --parameters   Show parameters of a model
+#       --system       Show system message of a model
+#       --template     Show template of a model
+#   -v, --verbose      Show detailed model information
+# 
+# Environment Variables:
+#       OLLAMA_HOST                IP Address for the ollama server (default 127.0.0.1:11434)
