59f5a1c0e266e2df757a8c69260719fc6df35adb
moljac <mcvjetko@holisticware.net>
2026-06-03 08:13:17 +0200

    Update serve.sh

--- 03-productivity/mac/topics/ai/hosts-providers-backends/lms-lm-studio/serve.sh
+++ 03-productivity/mac/topics/ai/hosts-providers-backends/lms-lm-studio/serve.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-source "${BASH_SOURCE%/*}/../ports.sh"
+# source "${BASH_SOURCE%/*}/../ports.sh"
 
-export LMS_LM_STUDIO_MODEL=qwen3.6-40b-claude-4.6-opus-deckard-heretic-uncensored-thinking
+export LMS_SERVER_PORT=11444
+
+export LMS_LM_STUDIO_MODEL=qwen3.6-35b-a3b
 
 lms \
     server \
