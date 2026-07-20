47573947bc111f416ac49cc5897ba305cb1fec82
moljac <mcvjetko@holisticware.net>
2026-07-20 23:13:06 +0200

    Update claude-with-llama.cpp.ps1

--- 01-system-integration/os-setup-config/windows/app-install/ps/winget/jana/bat.jana/claude-with-llama.cpp.ps1
+++ 01-system-integration/os-setup-config/windows/app-install/ps/winget/jana/bat.jana/claude-with-llama.cpp.ps1
@@ -2,4 +2,4 @@ $env:ANTHROPIC_AUTH_TOKEN = "llama.cpp"
 $env:ANTHROPIC_API_KEY = ""
 $env:ANTHROPIC_BASE_URL = "http://localhost:11454"
 $env:ANTHROPIC_MODEL = "Qwen3.6-27B"
-copilot --model $env:ANTHROPIC_MODEL
+claude --model $env:ANTHROPIC_MODEL
