06c98d5b34d998d7be3fc6657c145194cbad8c99
moljac <mcvjetko@holisticware.net>
2026-07-20 23:13:09 +0200

    Update claude-with-lms.ps1

--- 01-system-integration/os-setup-config/windows/app-install/ps/winget/jana/bat.jana/claude-with-lms.ps1
+++ 01-system-integration/os-setup-config/windows/app-install/ps/winget/jana/bat.jana/claude-with-lms.ps1
@@ -1,5 +1,5 @@
 $env:ANTHROPIC_AUTH_TOKEN = "lms--lm-studio"
 $env:ANTHROPIC_API_KEY = ""
 $env:ANTHROPIC_BASE_URL = "http://localhost:11444"
-$env:ANTHROPIC_MODEL = "qwen/qwen3.6-35b-a3b"
+$env:ANTHROPIC_MODEL = "google/gemma-4-31b-qat"
 claude --model $env:ANTHROPIC_MODEL
