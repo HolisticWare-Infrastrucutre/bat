542e4d2e97de582118c9e3318533debe23a439a6
moljac <mcvjetko@holisticware.net>
2026-09-18 20:05:18 +0200

    Update install-daily.sh

--- 01-system-integration/mac/dotnet/install-daily.sh
+++ 01-system-integration/mac/dotnet/install-daily.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+# https://dotnet.microsoft.com/en-us/download/dotnet
+# https://github.com/dotnet/installer/issues/11040
+# https://github.com/dotnet/dotnet/blob/main/docs/builds-table.md
+
 export OS=osx
 export EXT=pkg
 
@@ -14,7 +18,12 @@ export VERSIONS=\
 9.0.1xx
 9.0.2xx
 9.0.3xx
+9.0.4xx
 10.0
+10.0.1xx
+10.0.2xx
+10.0.3xx
+10.0.4xx
 "
 export PRODUCTS=\
 "
