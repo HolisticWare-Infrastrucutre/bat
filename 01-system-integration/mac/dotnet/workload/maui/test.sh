49befe32b817786dbd62fdae6db63ade67bd0eb1
moljac <mcvjetko@holisticware.net>
2026-09-18 21:31:42 +0200

    Update test.sh

--- 01-system-integration/mac/dotnet/workload/maui/test.sh
+++ 01-system-integration/mac/dotnet/workload/maui/test.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+export FOLDER=$HOME/Downloads/HolisticWare
+
     echo "---------------------------------------------------------------------------------------------------------------"
     echo \
     "
@@ -14,122 +16,122 @@ dev_android_emulator_launch \
 
 
     rm -fr \
-        $HOME/Downloads/dotnet/samples/AppMAUI01/
+        $FOLDER/dotnet/samples/AppMAUI01/
 
     dotnet new \
         mauiapp \
             --output \
-                $HOME/Downloads/dotnet/samples/AppMAUI01/
+                $FOLDER/dotnet/samples/AppMAUI01/
 
     dotnet build \
         --target:run \
         --framework net10.0-android \
-            $HOME/Downloads/dotnet/samples/AppMAUI01/
+            $FOLDER/dotnet/samples/AppMAUI01/
 
     dotnet build \
         --target:run \
         --framework net9.0-ios \
-            $HOME/Downloads/dotnet/samples/AppMAUI01/
+            $FOLDER/dotnet/samples/AppMAUI01/
 
     dotnet run \
         --framework net9.0-maccatalyst \
         --project \
-            $HOME/Downloads/dotnet/samples/AppMAUI01/
+            $FOLDER/dotnet/samples/AppMAUI01/
 
     rm -fr \
-        $HOME/Downloads/dotnet/samples/AppMAUI02/
+        $FOLDER/dotnet/samples/AppMAUI02/
         
     dotnet new \
         maui \
             --output \
-                $HOME/Downloads/dotnet/samples/AppMAUI02/
+                $FOLDER/dotnet/samples/AppMAUI02/
 
     dotnet build \
         --target:run \
         --framework net9.0-ios \
-            $HOME/Downloads/dotnet/samples/AppMAUI02/
+            $FOLDER/dotnet/samples/AppMAUI02/
 
     dotnet run \
         --framework net9.0-maccatalyst \
         --project \
-            $HOME/Downloads/dotnet/samples/AppMAUI02/
+            $FOLDER/dotnet/samples/AppMAUI02/
 
 
     rm -fr \
-        $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.BlazorWeb/
+        $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.BlazorWeb/
 
     dotnet new \
         maui-blazor-web \
             --output \
-                $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.BlazorWeb/
+                $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.BlazorWeb/
 
     dotnet build \
         --target:run \
         --framework net9.0-ios \
-            $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.BlazorWeb/
+            $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.BlazorWeb/
 
     dotnet run \
         --framework net9.0-maccatalyst \
         --project \
-            $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.BlazorWeb/
+            $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.BlazorWeb/
  
  
  
     rm -fr \
-        $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.Blazor/
+        $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.Blazor/
  
     dotnet new \
         maui-blazor \
             --output \
-                $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.Blazor/
+                $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.Blazor/
 
     dotnet build \
         --target:run \
         --framework net9.0-ios \
-            $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.Blazor/AppMAUI.MobileHybrid.BlazorWeb/
+            $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.Blazor/AppMAUI.MobileHybrid.BlazorWeb/
 
     dotnet run \
         --framework net9.0-maccatalyst \
         --project \
-            $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.Blazor/AppMAUI.MobileHybrid.BlazorWeb/
+            $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.Blazor/AppMAUI.MobileHybrid.BlazorWeb/
 
 
 
     rm -fr \
-        $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.BlazorFluentWeb/
+        $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.BlazorFluentWeb/
 
     dotnet \
         new \
             fluentmaui-blazor-web \
                 --framework net10.0 \
                 --output \
-                    $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.BlazorFluentWeb/
+                    $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.BlazorFluentWeb/
 
 
     dotnet build \
         --framework net10.0-ios \
-            $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.BlazorFluentWeb/AppMAUI.MobileHybrid.BlazorFluentWeb/
+            $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.BlazorFluentWeb/AppMAUI.MobileHybrid.BlazorFluentWeb/
 
     dotnet build \
         --target:run \
         --framework net10.0-ios \
-            $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.BlazorFluentWeb/AppMAUI.MobileHybrid.BlazorFluentWeb/
+            $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.BlazorFluentWeb/AppMAUI.MobileHybrid.BlazorFluentWeb/
  
     dotnet run \
         --framework net10.0-maccatalyst \
         --project \
-             $HOME/Downloads/dotnet/samples/AppMAUI.MobileHybrid.BlazorFluentWeb/AppMAUI.MobileHybrid.BlazorFluentWeb/
+             $FOLDER/dotnet/samples/AppMAUI.MobileHybrid.BlazorFluentWeb/AppMAUI.MobileHybrid.BlazorFluentWeb/
 
 
     dotnet \
         new \
             maui-multiproject \
                 --output \
-                    $HOME/Downloads/dotnet/samples/maui-multiproject/AppMAUI.MultiProject/
+                    $FOLDER/dotnet/samples/maui-multiproject/AppMAUI.MultiProject/
 
 
     dotnet \
         new \
             maui-multihead \
                 --output \
-                    $HOME/Downloads/dotnet/samples/maui-multiproject/AppMAUI.MultiHead/
+                    $FOLDER/dotnet/samples/maui-multiproject/AppMAUI.MultiHead/
