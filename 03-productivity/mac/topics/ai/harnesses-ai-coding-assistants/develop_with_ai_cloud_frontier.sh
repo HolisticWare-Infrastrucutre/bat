5ed0ec5e04ead4f4c60e495799ada35fc57e9d4f
moljac <mcvjetko@holisticware.net>
2026-09-25 22:55:24 +0200

    Create develop_with_ai_cloud_frontier.sh

---
+++ 03-productivity/mac/topics/ai/harnesses-ai-coding-assistants/develop_with_ai_cloud_frontier.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#export MODEL_NAME="Qwen3.6-35B-A3B"
+export MODEL_NAME="Qwen3.8-27B"
+
+export DIR_CURRENT=$(pwd)
+ttab \
+    -w \
+    -d $DIR_CURRENT \
+    claude
+
+ttab \
+    -d $DIR_CURRENT \
+    copilot
+
+ttab \
+    -d $DIR_CURRENT \
+    codex
+
+ttab \
+    -d $DIR_CURRENT \
+    qwen
+    
