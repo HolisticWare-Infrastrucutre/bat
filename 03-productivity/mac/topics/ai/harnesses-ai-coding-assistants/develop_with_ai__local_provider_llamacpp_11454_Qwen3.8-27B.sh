b2c53a6903d1d690cab4c0e68ef6ac6d23ccac07
moljac <mcvjetko@holisticware.net>
2026-09-25 22:55:20 +0200

    Create develop_with_ai__local_provider_llamacpp_11454_Qwen3.8-27B.sh

---
+++ 03-productivity/mac/topics/ai/harnesses-ai-coding-assistants/develop_with_ai__local_provider_llamacpp_11454_Qwen3.8-27B.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#export MODEL_NAME="Qwen3.6-35B-A3B"
+export MODEL_NAME="Qwen3.8-27B"
+export PORT=11454
+
+export DIR_CURRENT=$(pwd)
+
+ttab \
+    -w \
+    -d $DIR_CURRENT \
+    "source /Users/moljac/bat/03-productivity/mac/topics/ai/hosts-providers-backends/llama.cpp/serve-Qwen3.6-35B-A3B.sh"
+
+ttab \
+    -d $DIR_CURRENT \
+    "export ANTHROPIC_API_KEY=''; export ANTHROPIC_BASE_URL=http://localhost:$PORT; export ANTHROPIC_MODEL=$MODEL_NAME; claude --model $MODEL_NAME"
+
+ttab \
+    -d $DIR_CURRENT \
+    "export COPILOT_OFFLINE=true; export COPILOT_PROVIDER_API_KEY= ; export COPILOT_PROVIDER_BASE_URL=http://localhost:$PORT ; export COPILOT_MODEL=$MODEL_NAME; copilot"
+
+ttab \
+    -d $DIR_CURRENT \
+    opencode
+
+ttab \
+    -d $DIR_CURRENT \
+    hermes
+
+ttab \
+    -d $DIR_CURRENT \
+    "echo harnesses to the right have no plan mode"
+
+ttab \
+    -d $DIR_CURRENT \
+    pi
+
+ttab \
+    -d $DIR_CURRENT \
+    crush
+    
+ttab \
+    -d $DIR_CURRENT \
+    "export SMALLCODE_MODEL=$MODEL_NAME; export SMALLCODE_BASE_URL=http://localhost:$PORT/v1; smallcode"
+
